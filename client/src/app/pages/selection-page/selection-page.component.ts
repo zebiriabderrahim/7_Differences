55d8a0089d318559f3f6e72e9a5d89ea5483cc5f
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2023-01-19 23:25:04 -0500

    feat: add the selection page

---
+++ client/src/app/pages/selection-page/selection-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selection-page',
+  templateUrl: './selection-page.component.html',
+  styleUrls: ['./selection-page.component.scss']
+})
+export class SelectionPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
