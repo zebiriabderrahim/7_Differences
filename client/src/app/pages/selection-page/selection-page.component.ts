59c8d879d2e51d637191e54de62c90272d5e2897
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2023-01-21 21:16:52 -0500

    feat: added gameList fonctionality

--- client/src/app/pages/selection-page/selection-page.component.ts
+++ client/src/app/pages/selection-page/selection-page.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-selection-page',
-  templateUrl: './selection-page.component.html',
-  styleUrls: ['./selection-page.component.scss']
+    selector: 'app-selection-page',
+    templateUrl: './selection-page.component.html',
+    styleUrls: ['./selection-page.component.scss'],
 })
 export class SelectionPageComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+    titre = 'Selectionne ton jeux';
 
+    imageSrc: string = '../../../assets/img/rat.jpg';
+    newImageSrc: string = '../../../assets/img/strong_rat.jpg';
+
+    gameList = [
+        { name: 'titre jeu 1', picture: this.imageSrc },
+        { name: 'titre jeu 2', picture: this.imageSrc },
+        { name: 'titre jeu 3', picture: this.imageSrc },
+        { name: 'titre jeu 4', picture: this.imageSrc },
+        { name: 'titre jeu 5', picture: this.newImageSrc },
+        { name: 'titre jeu 6', picture: this.newImageSrc },
+        { name: 'titre jeu 7', picture: this.newImageSrc },
+        { name: 'titre jeu 8', picture: this.newImageSrc },
+    ];
+
+    gameIterator = 0;
+
+    previewAction() {
+        this.gameIterator--;
+        this.gameIterator--;
+        this.gameIterator--;
+        this.gameIterator--;
+    }
+
+    nextAction() {
+        this.gameIterator++;
+        this.gameIterator++;
+        this.gameIterator++;
+        this.gameIterator++;
+    }
+
+    selectedGame() {
+        alert('Va à la page du jeu selectionner');
+    }
 }
