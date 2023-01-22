237f10fc8c1d4c3365f052679aa977a111bd0b0b
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-01-21 22:00:23 -0500

    feat: created creation-page component

---
+++ client/src/app/pages/creation-page/creation-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreationPageComponent } from './creation-page.component';
+
+describe('CreationPageComponent', () => {
+  let component: CreationPageComponent;
+  let fixture: ComponentFixture<CreationPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreationPageComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CreationPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/pages/creation-page/creation-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creation-page',
+  templateUrl: './creation-page.component.html',
+  styleUrls: ['./creation-page.component.scss']
+})
+export class CreationPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
