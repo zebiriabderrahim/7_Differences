cb4b4df364ae310ada2c361e7989f77e6d8903f3
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2023-01-21 21:16:52 -0500

    feat: added gameList fonctionality

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+import { SelectionPageComponent } from '@app/pages/selection-page/selection-page.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
     { path: 'game', component: GamePageComponent },
     { path: 'material', component: MaterialPageComponent },
+    { path: 'selection', component: SelectionPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
