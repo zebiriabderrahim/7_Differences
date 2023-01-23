96e136959366dcf54fb89ed8d2373a03b8539bca
ZakariaZair <zakaria.zair@polymtl.ca>
2023-01-23 15:01:04 -0500

    fix: fixed typo in app.module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChronometerComponent } from '@app/components/chronometer/chronometer.component';
+import { GameInfosComponent } from '@app/components/game-infos/game-infos.component';
 import { GameSheetComponent } from '@app/components/game-sheet/game-sheet.component';
 import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
 import { PlayerNameDialogBoxComponent } from '@app/components/player-name-dialog-box/player-name-dialog-box.component';
@@ -14,9 +15,8 @@ import { AppComponent } from '@app/pages/app/app.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+import { SelectionPageComponent } from '@app/pages/selection-page/selection-page.component';
 import { SoloGameViewComponent } from '@app/pages/solo-game-view/solo-game-view.component';
-import { GameInfosComponent } from './components/game-infos/game-infos.component';
-import { SelectionPageComponent } from './pages/selection-page/selection-page.component';
 
 /**
  * Main module that is used in main.ts.
