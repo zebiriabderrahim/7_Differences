2fa246e95c44a308e698c1eac453c50631b142dc
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-01-23 11:34:43 -0500

    fix: changed path to game-infos

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChronometerComponent } from '@app/components/chronometer/chronometer.component';
+import { GameInfosComponent } from '@app/components/game-infos/game-infos.component';
+import { GameSheetComponent } from '@app/components/game-sheet/game-sheet.component';
 import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
+import { PlayerNameDialogBoxComponent } from '@app/components/player-name-dialog-box/player-name-dialog-box.component';
 import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
 import { AppRoutingModule } from '@app/modules/app-routing.module';
 import { AppMaterialModule } from '@app/modules/material.module';
@@ -12,10 +16,6 @@ import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { SoloGameViewComponent } from '@app/pages/solo-game-view/solo-game-view.component';
-import { ChronometerComponent } from '@app/components/chronometer/chronometer.component';
-import { GameSheetComponent } from '@app/components/game-sheet/game-sheet.component';
-import { PlayerNameDialogBoxComponent } from '@app/components/player-name-dialog-box/player-name-dialog-box.component';
-import { GameInfosComponent } from './game-infos/game-infos.component';
 
 /**
  * Main module that is used in main.ts.
