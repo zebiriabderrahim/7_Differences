d31bd2ab2d566ddf3778db56466296ce30b05ec6
ABDERRAHIM ZEBIRI <abderrahimzebiri@ABDERRAHIMs-MacBook-Pro.local>
2023-01-23 04:14:46 -0500

    fix : linting

--- client/src/app/game-infos/game-infos.component.spec.ts
+++ client/src/app/game-infos/game-infos.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GameInfosComponent } from './game-infos.component';
 
 describe('GameInfosComponent', () => {
-  let component: GameInfosComponent;
-  let fixture: ComponentFixture<GameInfosComponent>;
+    let component: GameInfosComponent;
+    let fixture: ComponentFixture<GameInfosComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ GameInfosComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [GameInfosComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(GameInfosComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(GameInfosComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- client/src/app/game-infos/game-infos.component.ts
+++ client/src/app/game-infos/game-infos.component.ts
@@ -1,16 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Game } from '@app/interfaces/game';
 
 @Component({
-  selector: 'app-game-infos',
-  templateUrl: './game-infos.component.html',
-  styleUrls: ['./game-infos.component.scss']
+    selector: 'app-game-infos',
+    templateUrl: './game-infos.component.html',
+    styleUrls: ['./game-infos.component.scss'],
 })
-export class GameInfosComponent implements OnInit {
-
-  @Input() game: Game
-
-  ngOnInit(): void {
-  }
-
+export class GameInfosComponent {
+    @Input() game: Game;
 }
