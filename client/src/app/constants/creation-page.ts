9ea6a98d4b54ee2c0e735cd417d40098bccd1dc8
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-01-23 17:15:32 -0500

    refactor: added constants

--- client/src/app/constants/creation-page.ts
+++ client/src/app/constants/creation-page.ts
@@ -2,6 +2,7 @@ export const IMG_WIDTH = 640;
 export const IMG_HEIGHT = 480;
 export const IMG_TYPE = 'data:image/bmp;';
 export const FORMAT_IMAGE = 24;
+export const BMP_HEADER_OFFSET = 28;
 export const DEFAULT_RADIUS = 3;
 // eslint-disable-next-line @typescript-eslint/no-magic-numbers
 export const RADIUS_SIZES = [0, 3, 9, 15];
