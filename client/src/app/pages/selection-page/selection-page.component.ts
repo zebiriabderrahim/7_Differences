6c8665964c9d8fbc2b0e074e8b732d573c1aafe0
ZakariaZair <zakaria.zair@polymtl.ca>
2023-01-23 10:45:44 -0500

    feat: changing selection-page construction

--- client/src/app/pages/selection-page/selection-page.component.ts
+++ client/src/app/pages/selection-page/selection-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Game } from '@app/interfaces/game';
 import { Gamecard } from '@app/interfaces/gamecard';
 
 @Component({
@@ -13,7 +14,7 @@ export class SelectionPageComponent {
     // eslint-disable-next-line no-alert, quotes, semi, @typescript-eslint/no-magic-numbers
     gamePhase: number = 4;
 
-    gameList: Gamecard[] = [
+    gameListTest: Gamecard[] = [
         { name: 'titre jeu 1', picture: this.imageSrc },
         { name: 'titre jeu 2', picture: this.imageSrc },
         { name: 'titre jeu 3', picture: this.imageSrc },
@@ -22,6 +23,13 @@ export class SelectionPageComponent {
         { name: 'titre jeu 6', picture: this.newImageSrc },
         { name: 'titre jeu 7', picture: this.newImageSrc },
         { name: 'titre jeu 8', picture: this.newImageSrc },
+    ];
+
+    gameList: Game[] = [
+        { id: 1, name: 'rat Game', difficultyLevel: 10, thumbnail: 'a', soloTopTime: [], oneVsOneTopTime: [], differencesCount: 15, hintList: [] },
+        { id: 1, name: 'rat Game 2', difficultyLevel: 15, thumbnail: 'b', soloTopTime: [], oneVsOneTopTime: [], differencesCount: 15, hintList: [] },
+        { id: 1, name: 'rat Game 3', difficultyLevel: 20, thumbnail: 'c', soloTopTime: [], oneVsOneTopTime: [], differencesCount: 15, hintList: [] },
+        { id: 1, name: 'rat Game 4', difficultyLevel: 25, thumbnail: 'd', soloTopTime: [], oneVsOneTopTime: [], differencesCount: 15, hintList: [] },
     ];
 
     hasPrevious: boolean = false;
