d1546f877b417883d4e692dd64d80c8d138bd949
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-01-23 16:57:16 -0500

    feat: added validateImageFormat fct

--- client/src/app/pages/creation-page/creation-page.component.ts
+++ client/src/app/pages/creation-page/creation-page.component.ts
@@ -8,25 +8,42 @@ import { DEFAULT_RADIUS, IMG_HEIGHT, IMG_TYPE, IMG_WIDTH, RADIUS_SIZES } from '@
 })
 export class CreationPageComponent {
     url: string = '';
+    isImageTypeValid: boolean = false;
+    isImageSizeValid: boolean = false;
+    isImageFormatValid: boolean = false;
 
     radiusSizes: number[] = RADIUS_SIZES;
     defaultRadius: number = DEFAULT_RADIUS;
 
-    isImageTypeValid(imageDescription: string) {
-        return imageDescription === IMG_TYPE;
+    validateImageType(imageDescription: string) {
+        this.isImageTypeValid = imageDescription.includes(IMG_TYPE);
     }
 
-    isImageSizeValid(event: Event) {
+    validateImageSize(event: Event) {
         const target = event.target as HTMLInputElement;
-        return target.width === IMG_WIDTH && target.height === IMG_HEIGHT;
+        this.isImageSizeValid = target.width === IMG_WIDTH && target.height === IMG_HEIGHT;
+    }
+
+    async validateImageFormat(file: File) {
+        const bmpHeader = new DataView(await file.arrayBuffer());
+        const bmpFormat = bmpHeader.getUint16(28, true);
+        this.isImageFormatValid = bmpFormat === 24;
     }
 
     onSelectFile(event: Event) {
         const target = event.target as HTMLInputElement;
         if (target.files && target.files[0]) {
             const reader = new FileReader();
+            const file = target.files[0];
             reader.readAsDataURL(target.files[0]);
             reader.onload = (e: Event) => {
+                const image = new Image();
+                image.src = reader.result as string;
+                image.onload = (ev: Event) => {
+                    this.validateImageType(image.src);
+                    this.validateImageSize(ev);
+                    this.validateImageFormat(file);
+                };
                 this.url = (e.target as FileReader).result as string;
             };
         }
