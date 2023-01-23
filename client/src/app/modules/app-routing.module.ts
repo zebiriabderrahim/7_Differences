edc0bcf066ec9a7e748b28cb0ba8e97c8f85083f
ZakariaZair <zakaria.zair@polymtl.ca>
2023-01-23 15:37:11 -0500

    feat: set up firstt config view

--- client/src/app/modules/app-routing.module.ts
+++ client/src/app/modules/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfigPageComponent } from '@app/pages/config-page/config-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { SelectionPageComponent } from '@app/pages/selection-page/selection-page.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
     { path: 'game', component: SoloGameViewComponent },
     { path: 'material', component: MaterialPageComponent },
     { path: 'selection', component: SelectionPageComponent },
+    { path: 'config', component: ConfigPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
