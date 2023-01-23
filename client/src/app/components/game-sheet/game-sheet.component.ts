853cf32a65dd9ffabe8ae7866af9d8a5d60708da
JeremyEar <jeremy_ear@hotmail.com>
2023-01-23 15:24:42 -0500

    fix: items location on game-sheet

--- client/src/app/components/game-sheet/game-sheet.component.ts
+++ client/src/app/components/game-sheet/game-sheet.component.ts
@@ -2,7 +2,6 @@ import { Component, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Game } from '@app/interfaces/game';
 import { PlayerNameDialogBoxComponent } from '@app/components/player-name-dialog-box/player-name-dialog-box.component';
-
 @Component({
     selector: 'app-game-sheet',
     templateUrl: './game-sheet.component.html',
@@ -14,8 +13,16 @@ export class GameSheetComponent {
         name: 'test',
         difficultyLevel: 1,
         thumbnail: 'test',
-        soloTopTime: [],
-        oneVsOneTopTime: [],
+        soloTopTime: [
+            { name: 'top1', time: 1 },
+            { name: 'top2', time: 2 },
+            { name: 'top3', time: 3 },
+        ],
+        oneVsOneTopTime: [
+            { name: 'test1', time: 1 },
+            { name: 'test2', time: 2 },
+            { name: 'test3', time: 3 },
+        ],
         differencesCount: 10,
         hintList: [],
     };
