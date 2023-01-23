7f3110b3a98b6cbc25a4ffdd212d47069fd3aa4d
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-01-23 13:54:02 -0500

    fix: linted constants

--- client/src/app/constants/creation-page.ts
+++ client/src/app/constants/creation-page.ts
@@ -3,4 +3,5 @@ export const IMG_HEIGHT = 480;
 export const IMG_TYPE = 'data:image/bmp;';
 export const FORMAT_IMAGE = 24;
 export const DEFAULT_RADIUS = 3;
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers
 export const RADIUS_SIZES = [0, 3, 9, 15];
