007835a822ce92e8d1feafd1a0b1e27d8f2470bf
ZakariaZair <zakaria.zair@polymtl.ca>
2023-01-23 20:59:31 -0500

    add test config page

--- client/src/app/pages/config-page/config-page.component.spec.ts
+++ client/src/app/pages/config-page/config-page.component.spec.ts
@@ -19,4 +19,19 @@ describe('ConfigPageComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('#nextFour() should display the next 4 games', () => {
+        const currentPhase = component.gameIterator;
+        expect(component.gameIterator).toEqual(currentPhase);
+        component.nextFour();
+        expect(component.gameIterator).toEqual(currentPhase + component.gamePhase);
+    });
+
+    it('#lastFour() should display the last 4 games', () => {
+        component.gameIterator += component.gamePhase;
+        const currentPhase = component.gameIterator;
+        expect(component.gameIterator).toEqual(currentPhase);
+        component.lastFour();
+        expect(component.gameIterator).toEqual(currentPhase - component.gamePhase);
+    });
 });
