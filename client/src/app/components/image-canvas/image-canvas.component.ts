95e4db594dfad29035691482d442b14915d5cc51
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-01-25 17:07:09 -0500

    feat: created image-canvas

---
+++ client/src/app/components/image-canvas/image-canvas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-canvas',
+  templateUrl: './image-canvas.component.html',
+  styleUrls: ['./image-canvas.component.scss']
+})
+export class ImageCanvasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
