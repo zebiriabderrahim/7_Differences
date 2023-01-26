be77e46bc2dac22e1ce533eaf39ff1479365d212
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-01-26 05:47:30 -0500

    feat: add 2 pictures

--- client/src/app/components/image-canvas/image-canvas.component.ts
+++ client/src/app/components/image-canvas/image-canvas.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 import { IMG_HEIGHT, IMG_WIDTH } from '@app/constants/creation-page';
 
 @Component({
@@ -6,7 +6,9 @@ import { IMG_HEIGHT, IMG_WIDTH } from '@app/constants/creation-page';
     templateUrl: './image-canvas.component.html',
     styleUrls: ['./image-canvas.component.scss'],
 })
-export class ImageCanvasComponent implements AfterViewInit {
+export class ImageCanvasComponent implements AfterViewInit, OnChanges {
+    @Input() position: string;
+    @Input() image: HTMLImageElement;
     @ViewChild('canvas')
     canvas: ElementRef = {} as ElementRef;
     context: CanvasRenderingContext2D;
@@ -15,8 +17,18 @@ export class ImageCanvasComponent implements AfterViewInit {
         this.canvas.nativeElement.width = IMG_WIDTH;
         this.canvas.nativeElement.height = IMG_HEIGHT;
         this.context = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
-        this.context.fillStyle = 'green';
-        this.context.fillRect(0, 0, IMG_WIDTH, IMG_HEIGHT);
+        this.context?.drawImage(this.image, 0, 0);
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        this.setCanvasImage(changes.image.currentValue);
+        // TODO: remove console.log
+        // eslint-disable-next-line no-console
+        console.log(changes.image.currentValue);
+    }
+
+    setCanvasImage(image: HTMLImageElement): void {
+        this.context?.drawImage(image, 0, 0);
     }
 
     resetCanvas(): void {
