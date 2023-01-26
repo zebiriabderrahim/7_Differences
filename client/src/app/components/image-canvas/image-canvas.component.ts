761b94ca7bd10fe40900092c99ef92c8bc06acc7
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-01-26 00:59:00 -0500

    feat: reset canvas

--- client/src/app/components/image-canvas/image-canvas.component.ts
+++ client/src/app/components/image-canvas/image-canvas.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IMG_HEIGHT, IMG_WIDTH } from '@app/constants/creation-page';
 
 @Component({
     selector: 'app-image-canvas',
     templateUrl: './image-canvas.component.html',
     styleUrls: ['./image-canvas.component.scss'],
 })
-export class ImageCanvasComponent {
+export class ImageCanvasComponent implements OnInit {
+    originalContextForeground: CanvasRenderingContext2D;
+    modifiableContextForeground: CanvasRenderingContext2D;
+    canvas: HTMLCanvasElement | null;
+    context: CanvasRenderingContext2D | null;
+
     // constructor() {}
-    // ngOnInit(): void {}
+    ngOnInit(): void {
+        this.canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
+        this.context = this.canvas.getContext('2d') as CanvasRenderingContext2D;
+        this.context.fillStyle = 'green';
+        this.context.fillRect(0, 0, IMG_WIDTH, IMG_HEIGHT);
+    }
+
+    resetCanvas(): void {
+        if (this.context) {
+            this.context.clearRect(0, 0, IMG_WIDTH, IMG_HEIGHT);
+        }
+    }
 }
