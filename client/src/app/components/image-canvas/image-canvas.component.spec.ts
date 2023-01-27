3046eed987bd37a729e5b55f3aab9fe855818356
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-01-27 00:14:52 -0500

    style: file lint

--- client/src/app/components/image-canvas/image-canvas.component.spec.ts
+++ client/src/app/components/image-canvas/image-canvas.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ImageCanvasComponent } from './image-canvas.component';
 
 describe('ImageCanvasComponent', () => {
-  let component: ImageCanvasComponent;
-  let fixture: ComponentFixture<ImageCanvasComponent>;
+    let component: ImageCanvasComponent;
+    let fixture: ComponentFixture<ImageCanvasComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ImageCanvasComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ImageCanvasComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(ImageCanvasComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(ImageCanvasComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
