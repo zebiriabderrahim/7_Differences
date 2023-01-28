5713cfab214c0ff781c10c94fda87cfa8015400a
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2023-01-27 20:59:45 -0500

    feat:Button Changing depending route

--- client/src/app/components/game-sheet/game-sheet.component.ts
+++ client/src/app/components/game-sheet/game-sheet.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { PlayerNameDialogBoxComponent } from '@app/components/player-name-dialog-box/player-name-dialog-box.component';
 import { Game } from '@app/interfaces/game-interfaces';
 
@@ -29,12 +30,26 @@ export class GameSheetComponent {
     };
 
     playerName: string;
-    constructor(public dialog: MatDialog) {}
+    buttonPlay: string;
+    buttonJoin: string;
+    constructor(public dialog: MatDialog, public router: Router) {}
 
     openDialog() {
-        const dialogRef = this.dialog.open(PlayerNameDialogBoxComponent);
-        dialogRef.afterClosed().subscribe((result) => {
-            this.playerName = result;
-        });
+        if (this.router.url === '/selection') {
+            const dialogRef = this.dialog.open(PlayerNameDialogBoxComponent);
+            dialogRef.afterClosed().subscribe((result) => {
+                this.playerName = result;
+            });
+        }
+    }
+
+    navigate() {
+        if (this.router.url === '/selection') {
+            this.buttonPlay = 'Jouer';
+            this.buttonJoin = 'Joindre';
+        } else if (this.router.url === '/config') {
+            this.buttonPlay = 'Supprimer';
+            this.buttonJoin = 'Réinitialier';
+        }
     }
 }
