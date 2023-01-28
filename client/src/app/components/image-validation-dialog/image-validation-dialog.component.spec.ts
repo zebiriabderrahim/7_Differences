12a88d91e3f3ed7545041fa6d5cee3bf1333fa04
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-01-28 00:08:14 -0500

    feat: created image-validation-dialog component

---
+++ client/src/app/components/image-validation-dialog/image-validation-dialog.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ImageValidationDialogComponent } from './image-validation-dialog.component';
+
+describe('ImageValidationDialogComponent', () => {
+  let component: ImageValidationDialogComponent;
+  let fixture: ComponentFixture<ImageValidationDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ImageValidationDialogComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ImageValidationDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/components/image-validation-dialog/image-validation-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-validation-dialog',
+  templateUrl: './image-validation-dialog.component.html',
+  styleUrls: ['./image-validation-dialog.component.scss']
+})
+export class ImageValidationDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
