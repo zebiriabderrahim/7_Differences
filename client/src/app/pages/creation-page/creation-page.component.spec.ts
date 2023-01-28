1a627709a43d892175fb4c37500a5e3a57b2a939
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-01-28 16:21:09 -0500

    test: fixed warnings from imports

--- client/src/app/pages/creation-page/creation-page.component.spec.ts
+++ client/src/app/pages/creation-page/creation-page.component.spec.ts
@@ -1,5 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { ImageCanvasComponent } from '@app/components/image-canvas/image-canvas.component';
 import { CreationPageComponent } from './creation-page.component';
 
 describe('CreationPageComponent', () => {
@@ -8,7 +12,13 @@ describe('CreationPageComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [CreationPageComponent],
+            declarations: [CreationPageComponent, ImageCanvasComponent],
+            imports: [MatDialogModule, MatRadioModule, MatIconModule, FormsModule],
+            providers: [
+                {
+                    provide: MatDialog,
+                },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(CreationPageComponent);
