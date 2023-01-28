8caea7ee114aa61770bfd3f9b70b7fb205deffef
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-01-28 14:07:00 -0500

    style: removed dead code from image service

--- client/src/app/services/image-service/image.service.ts
+++ client/src/app/services/image-service/image.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/member-ordering */
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
@@ -5,14 +6,6 @@ import { Subject } from 'rxjs';
     providedIn: 'root',
 })
 export class ImageService {
-    // private originalImageSource = new Subject<HTMLImageElement>();
-    // private modifiedImageSource = new Subject<HTMLImageElement>();
-
-    // originalImageObservable = this.originalImageSource.asObservable();
-    // modifiedImageObservable = this.modifiedImageSource.asObservable();
-
-    // originalImage: HTMLImageElement = new Image();
-    // modifiedImage: HTMLImageElement = new Image();
     private originalImageSource = new Subject<string>();
     private modifiedImageSource = new Subject<string>();
 
