c9d9c0fa5fc063b3e378ff3264a593815bf63439
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2023-01-30 18:32:02 -0500

    fix: removing useless tests

--- client/src/app/components/game-sheet/game-sheet.component.spec.ts
+++ client/src/app/components/game-sheet/game-sheet.component.spec.ts
@@ -1,34 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialog } from '@angular/material/dialog';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { PlayerNameDialogBoxComponent } from '../player-name-dialog-box/player-name-dialog-box.component';
 import { GameSheetComponent } from './game-sheet.component';
 
 describe('GameSheetComponent', () => {
     let component: GameSheetComponent;
     let fixture: ComponentFixture<GameSheetComponent>;
+    let dialog: MatDialog;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [RouterTestingModule],
-            declarations: [GameSheetComponent],
+            imports: [RouterTestingModule, MatDialog, PlayerNameDialogBoxComponent],
+            declarations: [GameSheetComponent, PlayerNameDialogBoxComponent],
             providers: [{ provide: MatDialog, useValue: {} }],
         }).compileComponents();
+    });
 
+    beforeEach(() => {
         fixture = TestBed.createComponent(GameSheetComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
+        dialog = TestBed.inject(MatDialog);
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
     });
 
-    it("shows the 'Jouer' text on the /selection page", () => {
+    it('should change the values of buttonPlay and ButtonJoin when url is /selection', () => {
+        spyOnProperty(component.router, 'url').and.returnValue('/selection');
+        component.navigate();
         expect(component.buttonPlay).toEqual('Jouer');
+        expect(component.buttonPlay).toEqual('Joindre');
     });
 
-    it("shows the 'Joindre' text on the /selection page", () => {
-        expect(component.buttonJoin).toEqual('Joindre');
+    it('should open the dialog', () => {
+        spyOn(dialog, 'open');
+        component.openDialog();
+        expect(dialog.open).toHaveBeenCalled();
     });
+
+    it('should call gameCard.redirection when a player name is not an empty string', () => {});
+
+    it('should not call gameCard.redirection when player name is an empty string', () => {});
 });
