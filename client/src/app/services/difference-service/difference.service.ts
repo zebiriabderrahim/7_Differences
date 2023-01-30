bb68a98cbbc3acd6c7e0670854d410f059e6f95a
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-01-30 16:59:36 -0500

    feat: added isCoordInImage fct

--- client/src/app/services/difference-service/difference.service.ts
+++ client/src/app/services/difference-service/difference.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { IMG_HEIGHT, IMG_WIDTH } from '@app/constants/creation-page';
+import { Vec2 } from '@app/interfaces/vec2';
+import { ImageService } from '@app/services/image-service/image.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class DifferenceService {
+    differencesArr: Vec2[] = [];
+    differencesCounter: number = this.differencesArr.length;
 
-  constructor() { }
+    constructor(public imageService: ImageService) {}
+
+    isCoordInImage(x: number, y: number): boolean {
+        return x >= 0 && x < IMG_WIDTH && y >= 0 && y < IMG_HEIGHT;
+    }
 }
