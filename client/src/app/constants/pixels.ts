b7d02f9b91f98e7cb725c9e156e13ad09b62883f
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-01-30 03:51:17 -0500

    feat: created pixels constants

---
+++ client/src/app/constants/pixels.ts
@@ -0,0 +1,13 @@
+import { Pixel } from '@app/interfaces/pixel';
+export const BLACK_PIXEL: Pixel = {
+    red: 0,
+    green: 0,
+    blue: 0,
+    alpha: 255,
+};
+export const WHITE_PIXEL: Pixel = {
+    red: 255,
+    green: 255,
+    blue: 255,
+    alpha: 255,
+};
