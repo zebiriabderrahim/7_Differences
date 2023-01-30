256ad483a24809a35559761412fea47bddf3d1ed
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-01-30 03:48:01 -0500

    feat: transform context to pixel array

--- client/src/app/services/image-service/image.service.ts
+++ client/src/app/services/image-service/image.service.ts
@@ -83,4 +83,30 @@ export class ImageService {
         this.setLeftBackground(image);
         this.setRightBackground(image);
     }
+    
+    transformContextToPixelArray(context: CanvasRenderingContext2D): Pixel[] {
+        const imageData = context.getImageData(0, 0, IMG_WIDTH, IMG_HEIGHT);
+        const data = imageData.data;
+        const pixelArray: Pixel[] = [];
+        for (let i = 0; i < data.length; i += 4) {
+            const pixel : Pixel = {
+                red: data[i],
+                green: data[i + 1],
+                blue: data[i + 2],
+                alpha: data[i + 3],
+            };
+            pixelArray.push(pixel);
+        }
+        console.log(pixelArray);
+        return pixelArray;
+    }
+
+    //Pixel[y][x] is the pixel at position (x,y)
+    transformPixelArrayToPixelMatrix(pixelArray: Pixel[]): Pixel[][] {
+        const pixelMatrix: Pixel[][] = [];
+        for (let i = 0; i < IMG_HEIGHT; i++) {
+            pixelMatrix.push(pixelArray.slice(i * IMG_WIDTH, (i + 1) * IMG_WIDTH));
+        }
+        return pixelMatrix;
+    }
 }
