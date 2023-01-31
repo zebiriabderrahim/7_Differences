acbd984051cda2d99f6eae5db2b4485cc1b7b394
ABDERRAHIM ZEBIRI <abderrahimzebiri@ABDERRAHIMs-MacBook-Pro.local>
2023-01-31 15:39:24 -0500

    fix: game list service

--- server/app/services/database/database.service.ts
+++ server/app/services/database/database.service.ts
@@ -1,53 +1,17 @@
-import { CreateGameDto } from '@app/model/dto/game/create-game.dto';
-import { GAME_CARROUSEL_SIZE } from '@common/constants';
-import { Game, GameCard, GameCarrousel, PlayerTime } from '@common/game-interfaces';
+import { Game, GameCard, CarrouselPaginator } from '@common/game-interfaces';
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
+import { GameListsService } from '@app/services/game-lists/game-lists.service';
 
 @Injectable()
 export class DatabaseService {
-    private createdGames: CreateGameDto[];
-    private games: Game[];
-    private gameCardsList: GameCard[];
-    private carrouselGames: GameCarrousel[];
-    private defaultBestTimes: PlayerTime[];
+    private games: Game[] = [];
+    private gameCardsList: GameCard[] = [];
+    private carrouselGames: CarrouselPaginator[] = [];
+    constructor(private gameListService: GameListsService) {}
 
-    constructor() {
-        this.defaultBestTimes = [
-            { name: 'rxjs', time: 100 },
-            { name: 'ts', time: 200 },
-            { name: 'angular', time: 250 },
-        ];
-        this.createdGames = [];
-        this.games = [
-            {
-                id: 1,
-                name: 'Bouffon',
-                difficultyLevel: 1,
-                thumbnail: 'test',
-                original: '@assets/Bouffon/original.bmp',
-                modified: '@assets/Bouffon/modified.bmp',
-                soloTopTime: [
-                    { name: 'top1', time: 1 },
-                    { name: 'top2', time: 2 },
-                    { name: 'top3', time: 3 },
-                ],
-                oneVsOneTopTime: [
-                    { name: 'test1', time: 1 },
-                    { name: 'test2', time: 2 },
-                    { name: 'test3', time: 3 },
-                ],
-                differencesCount: 10,
-                hintList: [],
-            },
-        ];
-        this.gameCardsList = [];
-        this.carrouselGames = [];
-    }
-
-    getGamesCarrousel(): GameCarrousel[] {
-        this.gameCardsList = this.buildGameCardsList();
-        this.carrouselGames = this.buildGameCarrouselList();
+    getGamesCarrousel(): CarrouselPaginator[] {
+        this.gameListService.buildGameCarrousel(this.gameCardsList, this.carrouselGames);
         return this.carrouselGames;
     }
 
@@ -67,29 +31,18 @@ export class DatabaseService {
 
     addGame(gameData: Game): void {
         this.games.push(gameData);
+        this.addGameCard(gameData);
     }
 
-    buildGameCarrouselList(): GameCarrousel[] {
-        for (let i = 0; i < this.gameCardsList.length; i += GAME_CARROUSEL_SIZE) {
-            const j = i;
-            const gameCarrousel: GameCarrousel = {
-                hasNext: i + GAME_CARROUSEL_SIZE < this.gameCardsList.length,
-                hasPrevious: j > 0,
-                gameCards: this.gameCardsList.slice(j, i + GAME_CARROUSEL_SIZE),
-            };
-            this.carrouselGames.push(gameCarrousel);
-        }
-        return this.carrouselGames;
-    }
-
-    buildGameCardsList(): GameCard[] {
-        return this.createdGames.map((game) => ({
+    addGameCard(game: Game): void {
+        const gameCard: GameCard = {
             id: game.id,
             name: game.name,
-            difficultyLevel: game.isHard,
-            soloTopTime: this.defaultBestTimes,
-            oneVsOneTopTime: this.defaultBestTimes,
+            difficultyLevel: game.difficultyLevel,
+            soloTopTime: game.soloTopTime,
+            oneVsOneTopTime: game.oneVsOneTopTime,
             thumbnail: game.thumbnail,
-        }));
+        };
+        this.gameCardsList.push(gameCard);
     }
 }
