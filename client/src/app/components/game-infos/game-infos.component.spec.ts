4b5fe9162b06ff53faea696021f05e017e21d9ca
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2023-02-01 17:01:37 -0500

    fix: merge with server-service

--- client/src/app/components/game-infos/game-infos.component.spec.ts
+++ client/src/app/components/game-infos/game-infos.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GameInfosComponent } from './game-infos.component';
 
 describe('GameInfosComponent', () => {
--- client/src/app/pages/solo-game-view/solo-game-view.component.spec.ts
+++ client/src/app/pages/solo-game-view/solo-game-view.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { SoloGameViewComponent } from './solo-game-view.component';
 
@@ -8,6 +9,7 @@ describe('SoloGameViewComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
             declarations: [SoloGameViewComponent],
         }).compileComponents();
 
