77f09f7485979db10b39c738e016854130b312f9
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-02-02 03:01:03 -0500

    feat: disply number of differences

--- client/src/app/components/creation-game-dialog/creation-game-dialog.component.ts
+++ client/src/app/components/creation-game-dialog/creation-game-dialog.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { IMG_HEIGHT, IMG_WIDTH } from '@app/constants/creation-page';
+import { DifferenceService } from '@app/services/difference-service/difference.service';
 import { ImageService } from '@app/services/image-service/image.service';
 
 @Component({
@@ -9,8 +10,11 @@ import { ImageService } from '@app/services/image-service/image.service';
 })
 export class CreationGameDialogComponent implements AfterViewInit {
     @ViewChild('differenceCanvas', { static: true }) differenceCanvas: ElementRef;
-    constructor(public imageService: ImageService) {}
+    constructor(public imageService: ImageService, private differenceService: DifferenceService) {}
 
+    get displayDifferences(): number {
+        return this.differenceService.differencePackages.length;
+    }
     ngAfterViewInit(): void {
         this.differenceCanvas.nativeElement.width = IMG_WIDTH;
         this.differenceCanvas.nativeElement.height = IMG_HEIGHT;
--- client/src/app/services/image-service/image.service.ts
+++ client/src/app/services/image-service/image.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { DEFAULT_RADIUS, IMG_HEIGHT, IMG_WIDTH } from '@app/constants/creation-page';
+import { IMG_HEIGHT, IMG_WIDTH } from '@app/constants/creation-page';
 import { BLACK_PIXEL, N_PIXEL_ATTRIBUTE, WHITE_PIXEL } from '@app/constants/pixels';
 import { CanvasPosition } from '@app/enum/canvas-position';
 import { Coordinate } from '@app/interfaces/coordinate';
 import { Pixel } from '@app/interfaces/pixel';
+import { DifferenceService } from '@app/services/difference-service/difference.service';
 
 @Injectable({
     providedIn: 'root',
@@ -15,7 +16,7 @@ export class ImageService {
     leftBackground: string = '';
     rightBackground: string = '';
 
-    enlargementRadius: number = DEFAULT_RADIUS;
+    constructor(public differenceService: DifferenceService) {}
 
     resetBackground(canvasPosition: CanvasPosition) {
         switch (canvasPosition) {
@@ -85,13 +86,13 @@ export class ImageService {
     }
 
     setEnlargementRadius(radius: number) {
-        this.enlargementRadius = radius;
+        this.differenceService.enlargementRadius = radius;
     }
 
     setDifferenceContext(context: CanvasRenderingContext2D) {
         this.differenceContext = context;
         if (this.leftBackground && this.rightBackground) {
-            this.validateDifferences(this.enlargementRadius);
+            this.validateDifferences();
         }
     }
 
@@ -116,22 +117,6 @@ export class ImageService {
         return pixelArray;
     }
 
-    generateDifferences(pixelArray1: Pixel[], pixelArray2: Pixel[]): Coordinate[] {
-        const differentCoordinates: Coordinate[] = [];
-        for (let i = 0; i < pixelArray1.length; i++) {
-            if (this.arePixelsDifferent(pixelArray1[i], pixelArray2[i])) {
-                const x = i % IMG_WIDTH;
-                const y = Math.floor(i / IMG_WIDTH);
-                differentCoordinates.push({ x, y });
-            }
-        }
-        return differentCoordinates;
-    }
-
-    arePixelsDifferent(pixel1: Pixel, pixel2: Pixel): boolean {
-        return !(pixel1.red === pixel2.red && pixel1.green === pixel2.green && pixel1.blue === pixel2.blue);
-    }
-
     transformPixelArrayToImageData(pixelArray: Pixel[]): Uint8ClampedArray {
         const data = new Uint8ClampedArray(IMG_WIDTH * IMG_HEIGHT * N_PIXEL_ATTRIBUTE);
         for (let i = 0; i < pixelArray.length; i++) {
@@ -142,37 +127,11 @@ export class ImageService {
         }
         return data;
     }
-
-    enlargeDifferences(differenceCoordinates: Coordinate[], radius: number): Coordinate[] {
-        const visitedDifferences: boolean[][] = new Array(IMG_WIDTH).fill(false).map(() => new Array(IMG_HEIGHT).fill(false)) as boolean[][];
-        const enlargedDifferenceCoordinates: Coordinate[] = [];
-        for (const coordinate of differenceCoordinates) {
-            for (let i = -radius; i <= radius; i++) {
-                for (let j = -radius; j <= radius; j++) {
-                    const distance = Math.sqrt(i * i + j * j);
-                    if (distance <= radius) {
-                        const largerCoordinate: Coordinate = { x: coordinate.x + i, y: coordinate.y + j };
-                        if (this.isCoordinateValid(largerCoordinate) && !visitedDifferences[largerCoordinate.x][largerCoordinate.y]) {
-                            enlargedDifferenceCoordinates.push(largerCoordinate);
-                            visitedDifferences[largerCoordinate.x][largerCoordinate.y] = true;
-                        }
-                    }
-                }
-            }
-        }
-        return enlargedDifferenceCoordinates;
-    }
-
-    isCoordinateValid(coordinate: Coordinate): boolean {
-        return coordinate.x >= 0 && coordinate.x < IMG_WIDTH && coordinate.y >= 0 && coordinate.y < IMG_HEIGHT;
-    }
-
-    validateDifferences(radius: number): void {
+    validateDifferences(): void {
         const leftPixelArray = this.transformContextToPixelArray(this.leftBackgroundContext);
         const rightPixelArray = this.transformContextToPixelArray(this.rightBackgroundContext);
-        const differenceCoordinates = this.generateDifferences(leftPixelArray, rightPixelArray);
-        const enlargedDifferences = this.enlargeDifferences(differenceCoordinates, radius);
-        this.drawDifferences(enlargedDifferences);
+        const differenceCoordinates = this.differenceService.generateDifferences(leftPixelArray, rightPixelArray);
+        this.drawDifferences(differenceCoordinates);
     }
 
     drawDifferences(differences: Coordinate[]): void {
