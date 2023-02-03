3d3bdd33a4c5234da27b4e9d9fbc53cf105403f7
ABDERRAHIM ZEBIRI <abderrahimzebiri@ABDERRAHIMs-MacBook-Pro.local>
2023-02-03 15:30:50 -0500

    fix: delete of initial commit examples

--- server/app/app.module.ts
+++ server/app/app.module.ts
@@ -1,18 +1,10 @@
 import { Logger, Module } from '@nestjs/common';
 // import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-// import { Course, courseSchema } from '@app/model/database/course';
-// import { CourseController } from '@app/controllers/course/course.controller';
-// import { CourseService } from '@app/services/course/course.service';
-// import { DateController } from '@app/controllers/date/date.controller';
-// import { ExampleController } from '@app/controllers/example/example.controller';
 // import { ChatGateway } from '@app/gateways/chat/chat.gateway';
-// import { DateService } from '@app/services/date/date.service';
-// import { ExampleService } from '@app/services/example/example.service';
 import { GameService } from './services/game/game.service';
 import { GameController } from './controllers/game/game.controller';
 import { DatabaseService } from './services/database/database.service';
-import { GameListsService } from './services/game-lists/game-lists.service';
 
 @Module({
     imports: [
@@ -26,9 +18,7 @@ import { GameListsService } from './services/game-lists/game-lists.service';
         // }),
         // MongooseModule.forFeature([{ name: Course.name, schema: courseSchema }]),
     ],
-    // controllers: [CourseController, DateController, ExampleController],
-    // providers: [ChatGateway, CourseService, DateService, ExampleService, Logger],
     controllers: [GameController],
-    providers: [Logger, GameService, DatabaseService, ConfigService, GameListsService],
+    providers: [Logger, GameService, DatabaseService, ConfigService],
 })
 export class AppModule {}
