3780d940bdfc71d5f9f0be9dbab343d25c546af8
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-02-02 19:58:19 -0500

    feat: added game name input feat

--- client/src/app/components/creation-game-dialog/creation-game-dialog.component.ts
+++ client/src/app/components/creation-game-dialog/creation-game-dialog.component.ts
@@ -1,5 +1,8 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 import { IMG_HEIGHT, IMG_WIDTH } from '@app/constants/creation-page';
+import { CreationPageComponent } from '@app/pages/creation-page/creation-page.component';
 import { DifferenceService } from '@app/services/difference-service/difference.service';
 import { ImageService } from '@app/services/image-service/image.service';
 
@@ -9,8 +12,18 @@ import { ImageService } from '@app/services/image-service/image.service';
     styleUrls: ['./creation-game-dialog.component.scss'],
 })
 export class CreationGameDialogComponent implements OnInit {
+    @Output() gameNameEvent = new EventEmitter<string>();
     @ViewChild('differenceCanvas', { static: true }) differenceCanvas: ElementRef;
-    constructor(public imageService: ImageService, private differenceService: DifferenceService) {}
+    gameName: string = '';
+    routerConfig: string = '/config/';
+    gameNameForm = new FormGroup({
+        name: new FormControl('', [Validators.required, Validators.pattern(/^\S*$/)]),
+    });
+    constructor(
+        public imageService: ImageService,
+        private differenceService: DifferenceService,
+        public dialogRef: MatDialogRef<CreationPageComponent>,
+    ) {}
 
     get displayDifferences(): number {
         return this.differenceService.differencePackages.length;
@@ -20,5 +33,16 @@ export class CreationGameDialogComponent implements OnInit {
         this.differenceCanvas.nativeElement.height = IMG_HEIGHT;
         const differenceContext = this.differenceCanvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
         this.imageService.setDifferenceContext(differenceContext);
+    }
+
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
+
+    submitForm() {
+        if (this.gameNameForm.valid && this.gameNameForm.value.name) {
+            this.gameNameEvent.emit(this.gameNameForm.value.name);
+            this.dialogRef.close();
+        }
     }
 }
