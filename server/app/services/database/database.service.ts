d2a7937f5ba202018accda334eeab17879c7f9ed
ABDERRAHIM ZEBIRI <abderrahimzebiri@ABDERRAHIMs-MacBook-Pro.local>
2023-02-03 23:12:28 -0500

    feat: add game lists service and update database service

--- server/app/services/database/database.service.ts
+++ server/app/services/database/database.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import { DEFAULT_COUNTDOWN_VALUE, DEFAULT_HINT_PENALTY, DEFAULT_BONUS_TIME, GAME_CARROUSEL_SIZE } from '@common/constants';
 import { CreateGameDto } from '@app/model/dto/game/create-game.dto';
+import { GameListsManagerService } from '@app/services/game-lists-manager/game-lists-manager.service';
 
 @Injectable()
 export class DatabaseService {
     private games: Game[] = [];
     private gameCardsList: GameCard[] = [];
-    private carrouselGames: CarouselPaginator[] = [];
+    private carouselGames: CarouselPaginator[] = [];
     private defaultBestTimes: PlayerTime[] = [
         { name: 'John Doe', time: 100 },
         { name: 'Jane Doe', time: 200 },
@@ -19,11 +20,9 @@ export class DatabaseService {
         penaltyTime: DEFAULT_HINT_PENALTY,
         bonusTime: DEFAULT_BONUS_TIME,
     };
-
+    constructor(private readonly gameListManager: GameListsManagerService) {}
     getGamesCarrousel(): CarouselPaginator[] {
-        this.carrouselGames = [];
-        this.buildGameCarrousel();
-        return this.carrouselGames;
+        return this.carouselGames;
     }
 
     getGameById(id: number): Game {
@@ -45,46 +44,15 @@ export class DatabaseService {
     }
 
     addGame(newGame: CreateGameDto): void {
-        const game = this.createGameFromGameDto(newGame);
+        const game = this.gameListManager.createGameFromGameDto(newGame);
         this.games.push(game);
         this.addGameCard(game);
     }
 
     addGameCard(game: Game): void {
-        const gameCard: GameCard = {
-            id: game.id,
-            name: game.name,
-            difficultyLevel: game.difficultyLevel,
-            soloTopTime: game.soloTopTime,
-            oneVsOneTopTime: game.oneVsOneTopTime,
-            thumbnail: game.thumbnail,
-        };
+        const gameCard = this.gameListManager.buildGameCardFromGame(game);
         this.gameCardsList.push(gameCard);
-    }
-
-    buildGameCarrousel(): void {
-        for (let i = 0; i < this.gameCardsList.length; i += GAME_CARROUSEL_SIZE) {
-            const j = i;
-            const gameCarrousel: CarouselPaginator = {
-                hasNext: i + GAME_CARROUSEL_SIZE < this.gameCardsList.length,
-                hasPrevious: j > 0,
-                gameCards: this.gameCardsList.slice(j, i + GAME_CARROUSEL_SIZE),
-            };
-            this.carrouselGames.push(gameCarrousel);
-        }
-    }
-    createGameFromGameDto(newGame: CreateGameDto): Game {
-        return {
-            id: newGame.id,
-            name: newGame.name,
-            original: newGame.originalImagePath,
-            modified: newGame.modifiedImagePath,
-            soloTopTime: this.defaultBestTimes,
-            oneVsOneTopTime: this.defaultBestTimes,
-            difficultyLevel: newGame.isHard,
-            thumbnail: newGame.originalImagePath,
-            differencesCount: newGame.nDifference,
-            hintList: [],
-        };
+        this.gameListManager.buildGameCarousel(this.gameCardsList, this.carouselGames);
+        this.gameListManager.addGameCarousel(gameCard, this.carouselGames);
     }
 }
