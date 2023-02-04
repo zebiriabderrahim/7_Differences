7773084b3ca85c96795d0ac84cd7af031599a96f
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2023-02-04 16:54:29 -0500

    fix: server linted

--- client/src/app/pages/main-page/main-page.component.spec.ts
+++ client/src/app/pages/main-page/main-page.component.spec.ts
@@ -2,20 +2,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
-// import { CommunicationService } from '@app/services/communication.service';
-// import { of, throwError } from 'rxjs';
-// import SpyObj = jasmine.SpyObj;
 
 describe('MainPageComponent', () => {
     let component: MainPageComponent;
     let fixture: ComponentFixture<MainPageComponent>;
-    // let communicationServiceSpy: SpyObj<CommunicationService>;
 
     beforeEach(async () => {
-        // communicationServiceSpy = jasmine.createSpyObj('ExampleService', ['basicGet', 'basicPost']);
-        // communicationServiceSpy.basicGet.and.returnValue(of({ title: '', body: '' }));
-        // communicationServiceSpy.basicPost.and.returnValue(of(new HttpResponse<string>({ status: 201, statusText: 'Created' })));
-
         await TestBed.configureTestingModule({
             imports: [RouterTestingModule, HttpClientModule],
             declarations: [MainPageComponent],
@@ -28,8 +20,6 @@ describe('MainPageComponent', () => {
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
-
-    // TODO: Les tests doivent être améliorés er bien représentés
 
     it('should create', () => {
         expect(component).toBeTruthy();
