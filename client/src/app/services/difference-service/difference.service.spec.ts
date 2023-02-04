45488d3cf80f2b12c936d4e237866c5acf25c271
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-02-04 00:36:17 -0500

    test: added isCoordinateValid tests

--- client/src/app/services/difference-service/difference.service.spec.ts
+++ client/src/app/services/difference-service/difference.service.spec.ts
@@ -13,4 +13,29 @@ describe('DifferenceService', () => {
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
+
+    it('isCoordinateValid should return true if the coordinate is valid', () => {
+        const coordinate = { x: 300, y: 200 };
+        expect(service.isCoordinateValid(coordinate)).toBe(true);
+    });
+
+    it('isCoordinateValid should return false if the x coordinate is negative', () => {
+        const coordinate = { x: -100, y: 200 };
+        expect(service.isCoordinateValid(coordinate)).toBe(false);
+    });
+
+    it('isCoordinateValid should return false if the x coordinate is greater than or equal to 640', () => {
+        const coordinate = { x: 640, y: 200 };
+        expect(service.isCoordinateValid(coordinate)).toBe(false);
+    });
+
+    it('isCoordinateValid should return false if the y coordinate is negative', () => {
+        const coordinate = { x: 300, y: -100 };
+        expect(service.isCoordinateValid(coordinate)).toBe(false);
+    });
+
+    it('isCoordinateValid should return false if the y coordinate is greater than or equal to 480', () => {
+        const coordinate = { x: 300, y: 480 };
+        expect(service.isCoordinateValid(coordinate)).toBe(false);
+    });
 });
