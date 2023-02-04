d2f7f032d3889f23cbadf4e6a5663aa1d14356f2
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2023-02-04 16:56:56 -0500

    fix: server linted

--- server/app/services/game-lists/game-lists.service.spec.ts
+++ server/app/services/game-lists/game-lists.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { GameListsService } from './game-lists.service';
 
 describe('GameListsService', () => {
-  let service: GameListsService;
+    let service: GameListsService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [GameListsService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [GameListsService],
+        }).compile();
 
-    service = module.get<GameListsService>(GameListsService);
-  });
+        service = module.get<GameListsService>(GameListsService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
