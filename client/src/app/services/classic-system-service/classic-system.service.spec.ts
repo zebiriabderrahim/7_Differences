125888b112d0df06b0a2bc64539b2101d12a0a5a
JeremyEar <jeremy_ear@hotmail.com>
2023-02-04 00:26:59 -0500

    update: start of tests for game-area-service

---
+++ client/src/app/services/classic-system-service/classic-system.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ClassicSystemService } from './classic-system.service';
+
+describe('ClassicSystemService', () => {
+  let service: ClassicSystemService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ClassicSystemService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
