c9d75d6738cb2e512db90b07373c9e8c5b022688
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-02-05 05:40:43 -0500

    test: started image-canvas

--- client/src/app/components/image-canvas/image-canvas.component.spec.ts
+++ client/src/app/components/image-canvas/image-canvas.component.spec.ts
@@ -2,11 +2,14 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { CanvasPosition } from '@app/enum/canvas-position';
+import { ImageService } from '@app/services/image-service/image.service';
 import { ImageCanvasComponent } from './image-canvas.component';
 
 describe('ImageCanvasComponent', () => {
     let component: ImageCanvasComponent;
     let fixture: ComponentFixture<ImageCanvasComponent>;
+    let imageService: ImageService;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -18,7 +21,7 @@ describe('ImageCanvasComponent', () => {
                 },
             ],
         }).compileComponents();
-
+        imageService = TestBed.inject(ImageService);
         fixture = TestBed.createComponent(ImageCanvasComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -26,5 +29,13 @@ describe('ImageCanvasComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('resetBackground should call imageService.resetBackground with the right Position', () => {
+        component.position = CanvasPosition.Left;
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        const imageServiceResetBackgroundSpy = spyOn(imageService, 'resetBackground').and.callFake(() => {});
+        component.resetBackground();
+        expect(imageServiceResetBackgroundSpy).toHaveBeenCalledWith(component.position);
     });
 });
