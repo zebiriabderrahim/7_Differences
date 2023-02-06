ac5828d8c933528791cf79d653b0cd1bc0931595
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-02-06 10:43:48 -0500

    test: added gameNameForm tests

--- client/src/app/components/creation-game-dialog/creation-game-dialog.component.spec.ts
+++ client/src/app/components/creation-game-dialog/creation-game-dialog.component.spec.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { CanvasTestHelper } from '@app/classes/canvas-test-helper';
 import { IMG_HEIGHT, IMG_WIDTH } from '@app/constants/creation-page';
+import { DifferenceService } from '@app/services/difference-service/difference.service';
 import { ImageService } from '@app/services/image-service/image.service';
 import { CreationGameDialogComponent } from './creation-game-dialog.component';
 
@@ -11,6 +12,7 @@ describe('CreationGameDialogComponent', () => {
     let component: CreationGameDialogComponent;
     let fixture: ComponentFixture<CreationGameDialogComponent>;
     let imageService: ImageService;
+    let differenceService: DifferenceService;
     let dialogRef: MatDialogRef<CreationGameDialogComponent, unknown>;
     let contextStub: CanvasRenderingContext2D;
 
@@ -26,6 +28,7 @@ describe('CreationGameDialogComponent', () => {
         fixture.detectChanges();
         dialogRef = TestBed.inject(MatDialogRef<CreationGameDialogComponent, unknown>);
         imageService = TestBed.inject(ImageService);
+        differenceService = TestBed.inject(DifferenceService);
         contextStub = CanvasTestHelper.createCanvas(IMG_WIDTH, IMG_HEIGHT).getContext('2d') as CanvasRenderingContext2D;
         imageService.leftBackgroundContext = contextStub;
         imageService.rightBackgroundContext = contextStub;
@@ -38,6 +41,34 @@ describe('CreationGameDialogComponent', () => {
     it('should initialize the gameNameForm', () => {
         expect(component.gameNameForm instanceof FormGroup).toBeTruthy();
         expect(component.gameNameForm.controls.name instanceof FormControl).toBeTruthy();
+    });
+
+    it('gameNameForm should be enabled isNumberOfDifferencesValid is true', () => {
+        differenceService.differencePackages = [
+            [
+                { x: 10, y: 20 },
+                { x: 30, y: 40 },
+            ],
+            [
+                { x: 50, y: 60 },
+                { x: 70, y: 80 },
+            ],
+            [
+                { x: 90, y: 100 },
+                { x: 110, y: 120 },
+            ],
+        ];
+        expect(component.gameNameForm.enabled).toBeTruthy();
+    });
+
+    it('gameNameForm should be disabled if isNumberOfDifferencesValid is false', () => {
+        differenceService.differencePackages = [
+            [
+                { x: 10, y: 20 },
+                { x: 30, y: 40 },
+            ],
+        ];
+        expect(component.gameNameForm.disabled).toBeFalsy();
     });
 
     it('should close the dialog onNoClick', () => {
