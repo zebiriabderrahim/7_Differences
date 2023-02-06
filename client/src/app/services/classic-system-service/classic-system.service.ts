393f77e82decb90cb7a29cc23bfc236969f69071
Jeremy Ear <jeremy_ear@hotmail.com>
2023-02-05 20:29:22 -0500

    update: start of classic-system-service

--- client/src/app/services/classic-system-service/classic-system.service.ts
+++ client/src/app/services/classic-system-service/classic-system.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
-// import { GameAreaService } from '@app/services/game-area-service/game-area.service';
+import { Game } from '@app/interfaces/game-interfaces';
+import { Vec2 } from '@app/interfaces/vec2';
+import { ClientSocketService } from '@app/services/client-socket-service/client-socket.service';
+import { GameAreaService } from '@app/services/game-area-service/game-area.service';
 @Injectable({
     providedIn: 'root',
 })
 export class ClassicSystemService {
-    // constructor(private gameAreaService: GameAreaService) {}
+    constructor(private gameAreaService: GameAreaService, private clientSocket: ClientSocketService) {}
+
+    requestVerification(game: Game, coords: Vec2) {
+        this.clientSocket.send('verifyCoords', { game, coords });
+    }
 }
