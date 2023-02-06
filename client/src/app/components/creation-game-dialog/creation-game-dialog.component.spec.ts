a24ee9533e3fdce10cef72ea6ddfa834d559786c
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-02-06 11:56:14 -0500

    test: added displayDifferences tests

--- client/src/app/components/creation-game-dialog/creation-game-dialog.component.spec.ts
+++ client/src/app/components/creation-game-dialog/creation-game-dialog.component.spec.ts
@@ -43,6 +43,57 @@ describe('CreationGameDialogComponent', () => {
         expect(component.gameNameForm.controls.name instanceof FormControl).toBeTruthy();
     });
 
+    it('should display the number of differences when displayDifferences is defined', () => {
+        differenceService.differencePackages.length = 5;
+        fixture.detectChanges();
+        const bannerElement: HTMLElement = fixture.nativeElement;
+        const p = bannerElement.querySelector('div > p');
+        expect(p?.textContent).toContain(component.displayDifferences);
+    });
+
+    it('should not display the number of differences when displayDifferences is not defined', () => {
+        fixture.detectChanges();
+        const bannerElement: HTMLElement = fixture.nativeElement;
+        const p = bannerElement.querySelector('div > p');
+        expect(p?.textContent).not.toContain(component.displayDifferences);
+    });
+
+    it('gameNameForm should be enabled isNumberOfDifferencesValid is true', () => {
+        differenceService.differencePackages = [
+            [
+                { x: 10, y: 20 },
+                { x: 30, y: 40 },
+            ],
+            [
+                { x: 50, y: 60 },
+                { x: 70, y: 80 },
+            ],
+            [
+                { x: 90, y: 100 },
+                { x: 110, y: 120 },
+            ],
+        ];
+        expect(component.gameNameForm.enabled).toBeTruthy();
+    });
+
+    it('gameNameForm should be enabled isNumberOfDifferencesValid is true', () => {
+        differenceService.differencePackages = [
+            [
+                { x: 10, y: 20 },
+                { x: 30, y: 40 },
+            ],
+            [
+                { x: 50, y: 60 },
+                { x: 70, y: 80 },
+            ],
+            [
+                { x: 90, y: 100 },
+                { x: 110, y: 120 },
+            ],
+        ];
+        expect(component.gameNameForm.enabled).toBeTruthy();
+    });
+
     it('gameNameForm should be enabled isNumberOfDifferencesValid is true', () => {
         differenceService.differencePackages = [
             [
@@ -70,6 +121,21 @@ describe('CreationGameDialogComponent', () => {
         ];
         expect(component.gameNameForm.disabled).toBeFalsy();
     });
+
+    // it('should display the warning message when isNumberOfDifferencesValid is false', () => {
+    //     differenceService.differencePackages.length = 5;
+    //     fixture.detectChanges();
+    //     const bannerElement: HTMLElement = fixture.nativeElement;
+    //     const p = bannerElement.querySelector('ng-template > p');
+    //     expect(p?.textContent).toEqual('Il doit y avoir entre 3 et 9 différences. Veuillez recommencer le processus');
+    // });
+
+    // it('should not display the warning message when isNumberOfDifferencesValid is true', () => {
+    //     differenceService.differencePackages.length = 1;
+    //     const bannerElement: HTMLElement = fixture.nativeElement;
+    //     const p = bannerElement.querySelector('ng-template > p');
+    //     expect(p).toBeUndefined();
+    // });
 
     it('should close the dialog onNoClick', () => {
         component.onNoClick();
