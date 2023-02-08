e6b894e47a614cdc793d43f57e062255d9d025fc
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-02-08 16:11:38 -0500

    feat: created canvas-under-buttons

---
+++ client/src/app/components/canvas-under-buttons/canvas-under-buttons.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CanvasUnderButtonsComponent } from './canvas-under-buttons.component';
+
+describe('CanvasUnderButtonsComponent', () => {
+  let component: CanvasUnderButtonsComponent;
+  let fixture: ComponentFixture<CanvasUnderButtonsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CanvasUnderButtonsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CanvasUnderButtonsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/components/canvas-under-buttons/canvas-under-buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas-under-buttons',
+  templateUrl: './canvas-under-buttons.component.html',
+  styleUrls: ['./canvas-under-buttons.component.scss']
+})
+export class CanvasUnderButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
