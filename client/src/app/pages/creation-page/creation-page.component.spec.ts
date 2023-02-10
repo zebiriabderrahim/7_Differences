dd53504cea9ff8dd434d962f9c8e4cad21dc584c
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-02-10 17:58:52 -0500

    test: added radio button test

--- client/src/app/pages/creation-page/creation-page.component.spec.ts
+++ client/src/app/pages/creation-page/creation-page.component.spec.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { MatDialog, MatDialogConfig, MatDialogModule } from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
 import { MatRadioModule } from '@angular/material/radio';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CanvasUnderButtonsComponent } from '@app/components/canvas-under-buttons/canvas-under-buttons.component';
 import { CreationGameDialogComponent } from '@app/components/creation-game-dialog/creation-game-dialog.component';
@@ -56,19 +57,10 @@ describe('CreationPageComponent', () => {
         expect(matDialogSpy.open).toHaveBeenCalled();
     });
 
-    // it('should display the number of differences when displayDifferences is defined', () => {
-    //     differenceService.differencePackages.length = 5;
-    //     fixture.detectChanges();
-    //     const bannerElement: HTMLElement = fixture.nativeElement;
-    //     const p = bannerElement.querySelector('div > p');
-    //     expect(p?.textContent).toContain(component.displayDifferences);
-    // });
-
-    // it('should navigate to the Configuration page when clicked', () => {
-    //     const bannerElement: HTMLElement = fixture.nativeElement;
-    //     const buttonElement = bannerElement.querySelector('div > button');
-    //     const navigateSpy = spyOn(router, 'navigate').and.stub();
-    //     buttonElement.click();
-    //     expect(navigateSpy).toHaveBeenCalledWith(['configRoute']);
-    // });
+    it('should select a radio button', () => {
+        const radioButtons = fixture.debugElement.query(By.css('mat-radio-button')).nativeElement;
+        radioButtons.click();
+        fixture.detectChanges();
+        expect(component.radius).toEqual(component.radiusSizes[1]);
+    });
 });
