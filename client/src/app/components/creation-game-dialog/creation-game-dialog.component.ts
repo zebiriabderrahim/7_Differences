c2dce9971f6bed8bd1a6477e6487f6a1e4b3fe06
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-02-10 16:23:35 -0500

    fix: image service attributes set to private

--- client/src/app/components/creation-game-dialog/creation-game-dialog.component.ts
+++ client/src/app/components/creation-game-dialog/creation-game-dialog.component.ts
@@ -43,7 +43,7 @@ export class CreationGameDialogComponent implements OnInit {
         this.gameName = '';
         this.differenceCanvas.nativeElement.width = IMG_WIDTH;
         this.differenceCanvas.nativeElement.height = IMG_HEIGHT;
-        this.imageService.differenceContext = this.differenceCanvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
+        this.imageService.setDifferenceContext(this.differenceCanvas.nativeElement.getContext('2d') as CanvasRenderingContext2D);
     }
 
     isNumberOfDifferencesValid(): boolean {
