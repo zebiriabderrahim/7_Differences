7002ef2965dd92183a1f75a24c42901cc76d4635
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-02-10 21:11:48 -0500

    test: added isImageSizeValid tests

--- client/src/app/components/canvas-under-buttons/canvas-under-buttons.component.spec.ts
+++ client/src/app/components/canvas-under-buttons/canvas-under-buttons.component.spec.ts
@@ -6,6 +6,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { IMG_HEIGHT, IMG_WIDTH } from '@app/constants/creation-page';
 // import { IMG_HEIGHT, IMG_TYPE, IMG_WIDTH } from '@app/constants/creation-page';
 import { CanvasPosition } from '@app/enum/canvas-position';
 import { ImageService } from '@app/services/image-service/image.service';
@@ -38,7 +39,7 @@ describe('CanvasUnderButtonsComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    it('set ImageIfValid should open the invalidImageDialog when given an invalid type image and should not set the image', () => {
+    it('setImageIfValid should open the invalidImageDialog when given an invalid type image and should not set the image', () => {
         spyOn(validationService, 'isImageTypeValid').and.callFake(() => {
             return false;
         });
@@ -70,81 +71,13 @@ describe('CanvasUnderButtonsComponent', () => {
         expect(imageServiceResetBackgroundSpy).toHaveBeenCalledWith(component.position);
     });
 
-    // it('isImageSizeValid should return true when given the right image size', () => {
-    //     const event: Event = {
-    //         target: { width: IMG_WIDTH, height: IMG_HEIGHT } as HTMLInputElement,
-    //         bubbles: false,
-    //         cancelBubble: false,
-    //         cancelable: false,
-    //         composed: false,
-    //         currentTarget: null,
-    //         defaultPrevented: false,
-    //         eventPhase: 0,
-    //         isTrusted: false,
-    //         returnValue: false,
-    //         srcElement: null,
-    //         timeStamp: 0,
-    //         type: '',
-    //         composedPath(): EventTarget[] {
-    //             throw new Error('Function not implemented.');
-    //         },
-    //         // eslint-disable-next-line no-unused-vars
-    //         initEvent(_type: string, _bubbles?: boolean | undefined, cancelable?: boolean | undefined): void {
-    //             throw new Error('Function not implemented.');
-    //         },
-    //         preventDefault(): void {
-    //             throw new Error('Function not implemented.');
-    //         },
-    //         stopImmediatePropagation(): void {
-    //             throw new Error('Function not implemented.');
-    //         },
-    //         stopPropagation(): void {
-    //             throw new Error('Function not implemented.');
-    //         },
-    //         AT_TARGET: 0,
-    //         BUBBLING_PHASE: 0,
-    //         CAPTURING_PHASE: 0,
-    //         NONE: 0,
-    //     };
-    //     expect(component.isImageSizeValid(event)).toBeTruthy();
-    // });
+    it('isImageSizeValid should return true when given the right image size', () => {
+        const image = { width: IMG_WIDTH, height: IMG_HEIGHT } as HTMLImageElement;
+        expect(validationService.isImageSizeValid(image)).toBeTruthy();
+    });
 
-    // it('isImageSizeValid should return false when given the wrong image size', () => {
-    //     const event: Event = {
-    //         target: { width: IMG_WIDTH + 1, height: IMG_HEIGHT - 1 } as HTMLInputElement,
-    //         bubbles: false,
-    //         cancelBubble: false,
-    //         cancelable: false,
-    //         composed: false,
-    //         currentTarget: null,
-    //         defaultPrevented: false,
-    //         eventPhase: 0,
-    //         isTrusted: false,
-    //         returnValue: false,
-    //         srcElement: null,
-    //         timeStamp: 0,
-    //         type: '',
-    //         composedPath(): EventTarget[] {
-    //             throw new Error('Function not implemented.');
-    //         },
-    //         // eslint-disable-next-line no-unused-vars
-    //         initEvent(_type: string, _bubbles?: boolean | undefined, cancelable?: boolean | undefined): void {
-    //             throw new Error('Function not implemented.');
-    //         },
-    //         preventDefault(): void {
-    //             throw new Error('Function not implemented.');
-    //         },
-    //         stopImmediatePropagation(): void {
-    //             throw new Error('Function not implemented.');
-    //         },
-    //         stopPropagation(): void {
-    //             throw new Error('Function not implemented.');
-    //         },
-    //         AT_TARGET: 0,
-    //         BUBBLING_PHASE: 0,
-    //         CAPTURING_PHASE: 0,
-    //         NONE: 0,
-    //     };
-    //     expect(component.isImageSizeValid(event)).toBeFalsy();
-    // });
+    it('isImageSizeValid should return false when given the wrong image size', () => {
+        const image = { width: IMG_WIDTH + 1, height: IMG_HEIGHT - 1 } as HTMLImageElement;
+        expect(validationService.isImageSizeValid(image)).toBeFalsy();
+    });
 });
