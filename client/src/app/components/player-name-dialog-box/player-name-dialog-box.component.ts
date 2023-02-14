ab8c42f70f843f2fd824d4961d83698ce20ea19c
ZakariaZair <89856615+ZakariaZair@users.noreply.github.com>
2023-02-13 23:49:43 -0500

    feat: games fetch from server synchronisation after creating games

--- client/src/app/components/player-name-dialog-box/player-name-dialog-box.component.ts
+++ client/src/app/components/player-name-dialog-box/player-name-dialog-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
@@ -8,9 +8,6 @@ import { MatDialogRef } from '@angular/material/dialog';
     styleUrls: ['./player-name-dialog-box.component.scss'],
 })
 export class PlayerNameDialogBoxComponent {
-    @Output() playerNameEvent = new EventEmitter<string>();
-    playerName: string = '';
-
     playerNameForm = new FormGroup({
         name: new FormControl('', [Validators.required, Validators.pattern(/^\S*$/)]),
     });
@@ -23,7 +20,6 @@ export class PlayerNameDialogBoxComponent {
 
     submitForm() {
         if (this.playerNameForm.valid && this.playerNameForm.value.name) {
-            this.playerNameEvent.emit(this.playerNameForm.value.name);
             this.dialogRef.close(this.playerNameForm.value.name);
         }
     }
