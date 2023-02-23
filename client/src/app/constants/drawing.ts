5e43c07fa0163a6f41304963709242a6b1d50992
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-02-23 02:09:32 -0500

    feat: initial foreground modification created

---
+++ client/src/app/constants/drawing.ts
@@ -0,0 +1,6 @@
+export const COLORS: string[] = ['white', 'black', 'red', 'green', 'blue', 'yellow', 'orange', 'purple', 'brown', 'grey'];
+export const DEFAULT_COLOR = 'black';
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers -- draw values for the canvas
+export const DRAW_VALUES: number[] = [4, 6, 8, 10, 12, 14, 16, 18, 20];
+export const DEFAULT_ERASER_VALUE = 4;
+export const DEFAULT_PENCIL_VALUE = 4;
