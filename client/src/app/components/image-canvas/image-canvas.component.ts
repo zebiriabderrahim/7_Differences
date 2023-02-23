8f8d7f2c3f25c9ce08c01c333b326bdfa58b05d8
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-02-23 02:09:32 -0500

    feat: initial foreground modification created

--- client/src/app/components/image-canvas/image-canvas.component.ts
+++ client/src/app/components/image-canvas/image-canvas.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { IMG_HEIGHT, IMG_WIDTH } from '@app/constants/image';
+import { COLORS, DEFAULT_COLOR, DRAW_VALUES, DEFAULT_ERASER_VALUE, DEFAULT_PENCIL_VALUE } from '@app/constants/drawing';
 import { CanvasPosition } from '@app/enum/canvas-position';
 import { ImageService } from '@app/services/image-service/image.service';
 
@@ -11,13 +12,59 @@ import { ImageService } from '@app/services/image-service/image.service';
 export class ImageCanvasComponent implements AfterViewInit {
     @Input() position: CanvasPosition;
     @ViewChild('backgroundCanvas') backgroundCanvas: ElementRef;
+    @ViewChild('foregroundCanvas') foregroundCanvas: ElementRef;
     context: CanvasRenderingContext2D;
+    readonly canvasSizes = { width: IMG_WIDTH, height: IMG_HEIGHT };
+    drawColor: string = DEFAULT_COLOR;
+    isColorSelected: boolean = false;
+    colors: string[] = COLORS;
+    drawValues: number[] = DRAW_VALUES;
+    pencilDiameter: number;
+    eraserLength: number;
+    isDragging: boolean = false;
 
-    constructor(private readonly imageService: ImageService) {}
+    constructor(private readonly imageService: ImageService) {
+        this.pencilDiameter = DEFAULT_PENCIL_VALUE;
+        this.eraserLength = DEFAULT_ERASER_VALUE;
+    }
     ngAfterViewInit(): void {
-        this.backgroundCanvas.nativeElement.width = IMG_WIDTH;
-        this.backgroundCanvas.nativeElement.height = IMG_HEIGHT;
-        this.context = this.backgroundCanvas.nativeElement.getContext('2d', { willReadFrequently: true }) as CanvasRenderingContext2D;
-        this.imageService.setBackgroundContext(this.position, this.context);
+        const backgroundContext: CanvasRenderingContext2D = this.backgroundCanvas.nativeElement.getContext('2d', { willReadFrequently: true });
+        this.imageService.setBackgroundContext(this.position, backgroundContext);
+    }
+
+    startCanvasOperation(event: MouseEvent): void {
+        const canvas = this.foregroundCanvas.nativeElement;
+        const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+        context.beginPath();
+        context.moveTo(event.offsetX, event.offsetY);
+        context.strokeStyle = this.drawColor;
+        context.lineWidth = this.pencilDiameter;
+        context.lineCap = 'round';
+        context.lineJoin = 'round';
+        context.stroke();
+        this.isDragging = true;
+    }
+
+    continueCanvasOperation(event: MouseEvent): void {
+        if (this.isDragging) {
+            const canvas = this.foregroundCanvas.nativeElement;
+            const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+            context.lineTo(event.offsetX, event.offsetY);
+            context.strokeStyle = this.drawColor;
+            context.lineWidth = this.pencilDiameter;
+            context.lineCap = 'round';
+            context.lineJoin = 'round';
+            context.stroke();
+        }
+    }
+
+    stopCanvasOperation(event: MouseEvent): void {
+        if (this.isDragging) {
+            const canvas = this.foregroundCanvas.nativeElement;
+            const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+            context.lineTo(event.offsetX, event.offsetY);
+            context.closePath();
+            this.isDragging = false;
+        }
     }
 }
--- client/src/app/modules/material.module.ts
+++ client/src/app/modules/material.module.ts
@@ -20,7 +20,7 @@ import { MatInputModule } from '@angular/material/input';
 import { HttpClientModule } from '@angular/common/http';
 import { MatRadioModule } from '@angular/material/radio';
 import { BrowserModule } from '@angular/platform-browser';
-// import { MatSelectModule } from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 // import { MatSidenavModule } from '@angular/material/sidenav';
 // import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 // import { MatSliderModule } from '@angular/material/slider';
@@ -55,7 +55,7 @@ const modules = [
     // MatProgressBarModule,
     // MatProgressSpinnerModule,
     MatRadioModule,
-    // MatSelectModule,
+    MatSelectModule,
     // MatSidenavModule,
     // MatSliderModule,
     // MatSlideToggleModule,
