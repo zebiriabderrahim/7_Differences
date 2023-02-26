da2b1151afdb199b3de01fb760b92c3e8526f53d
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-02-26 01:33:41 -0500

    feat: draw rectangle first iteration

--- client/src/app/services/draw-service/draw.service.ts
+++ client/src/app/services/draw-service/draw.service.ts
@@ -4,6 +4,7 @@ import { IMG_HEIGHT, IMG_WIDTH } from '@app/constants/image';
 import { CanvasAction } from '@app/enum/canvas-action';
 import { CanvasPosition } from '@app/enum/canvas-position';
 import { CanvasOperation } from '@app/interfaces/canvas-operation';
+import { Coordinate } from '@common/coordinate';
 
 @Injectable({
     providedIn: 'root',
@@ -13,14 +14,12 @@ export class DrawService {
     private rightForegroundContext: CanvasRenderingContext2D;
     private activeContext: CanvasRenderingContext2D;
     private activeCanvas: CanvasPosition;
-    // private operationWidth: number;
     private isDragging: boolean;
-    // private drawingColor: string;
+    private rectangleTopCorner: Coordinate;
+    private currentAction: CanvasAction;
 
     constructor() {
         this.isDragging = false;
-        // this.drawingColor = 'black';
-        // this.operationWidth = DEFAULT_PENCIL_VALUE;
     }
 
     // TODO: possible to avoid repetition?
@@ -59,41 +58,68 @@ export class DrawService {
     }
 
     setCanvasOperationStyle(canvasAction: CanvasAction, color: string, operationWidth: number) {
-        this.activeContext.lineWidth = operationWidth;
-        switch (canvasAction) {
-            case CanvasAction.Pencil:
-                this.activeContext.strokeStyle = color;
-                this.activeContext.lineCap = 'round';
-                this.activeContext.lineJoin = 'round';
-                break;
-            case CanvasAction.Eraser:
-                this.activeContext.strokeStyle = ERASER_COLOR;
-                this.activeContext.lineCap = 'square';
-                this.activeContext.lineJoin = 'round';
-                break;
+        if (canvasAction === CanvasAction.Rectangle) {
+            this.activeContext.fillStyle = color;
+        } else {
+            this.activeContext.lineWidth = operationWidth;
+            switch (canvasAction) {
+                case CanvasAction.Pencil:
+                    this.activeContext.strokeStyle = color;
+                    this.activeContext.lineCap = 'round';
+                    this.activeContext.lineJoin = 'round';
+                    break;
+                case CanvasAction.Eraser:
+                    this.activeContext.strokeStyle = ERASER_COLOR;
+                    this.activeContext.lineCap = 'square';
+                    this.activeContext.lineJoin = 'round';
+                    break;
+            }
         }
     }
 
     startCanvasOperation(canvasOperation: CanvasOperation, event: MouseEvent) {
+        this.currentAction = canvasOperation.action;
         this.setActiveCanvas(canvasOperation.position);
         this.setCanvasOperationStyle(canvasOperation.action, canvasOperation.color, canvasOperation.width);
-        this.activeContext.beginPath();
-        this.activeContext.lineTo(event.offsetX, event.offsetY);
-        this.activeContext.stroke();
+        if (canvasOperation.action === CanvasAction.Rectangle) {
+            this.rectangleTopCorner = { x: event.offsetX, y: event.offsetY };
+        } else {
+            this.activeContext.beginPath();
+            this.activeContext.lineTo(event.offsetX, event.offsetY);
+            this.activeContext.stroke();
+        }
         this.isDragging = true;
     }
 
     continueCanvasOperation(canvasPosition: CanvasPosition, event: MouseEvent) {
         if (this.isDragging && this.activeCanvas === canvasPosition) {
-            this.activeContext.lineTo(event.offsetX, event.offsetY);
-            this.activeContext.stroke();
+            if (this.currentAction === CanvasAction.Rectangle) {
+                this.activeContext.fillRect(
+                    this.rectangleTopCorner.x,
+                    this.rectangleTopCorner.y,
+                    event.offsetX - this.rectangleTopCorner.x,
+                    event.offsetY - this.rectangleTopCorner.y,
+                );
+            } else {
+                this.activeContext.lineTo(event.offsetX, event.offsetY);
+                this.activeContext.stroke();
+            }
         }
     }
 
     stopCanvasOperation(canvasPosition: CanvasPosition, event: MouseEvent) {
         if (this.isDragging) {
-            this.activeContext.lineTo(event.offsetX, event.offsetY);
-            this.activeContext.closePath();
+            if (this.currentAction === CanvasAction.Rectangle) {
+                this.activeContext.fillRect(
+                    this.rectangleTopCorner.x,
+                    this.rectangleTopCorner.y,
+                    event.offsetX - this.rectangleTopCorner.x,
+                    event.offsetY - this.rectangleTopCorner.y,
+                );
+            } else {
+                this.activeContext.lineTo(event.offsetX, event.offsetY);
+                this.activeContext.stroke();
+            }
             this.isDragging = false;
         }
     }
