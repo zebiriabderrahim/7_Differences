1f21904d7c2bccd1075a1581cc25d91d32d00312
ABDERRAHIM ZEBIRI <abderrahimzebiri@ABDERRAHIMs-MacBook-Pro.local>
2023-02-25 20:51:48 -0500

    fix: update async methodes in game-lists  service and their teste's

--- server/app/services/game-lists-manager/game-lists-manager.service.spec.ts
+++ server/app/services/game-lists-manager/game-lists-manager.service.spec.ts
@@ -1,9 +1,8 @@
 import { Game } from '@app/model/database/game';
-import { CreateGameDto } from '@app/model/dto/game/create-game.dto';
-import { CarouselPaginator, GameCard, PlayerTime, ServerSideGame } from '@common/game-interfaces';
+import { CarouselPaginator, GameCard, PlayerTime } from '@common/game-interfaces';
 import { Test, TestingModule } from '@nestjs/testing';
-import { GameListsManagerService } from './game-lists-manager.service';
 import * as fs from 'fs';
+import { GameListsManagerService } from './game-lists-manager.service';
 
 describe('GameListsManagerService', () => {
     let service: GameListsManagerService;
@@ -13,50 +12,30 @@ describe('GameListsManagerService', () => {
         { name: 'Jane Doe', time: 200 },
         { name: 'the scream', time: 250 },
     ];
-    const testCarousel: CarouselPaginator[] = [
-        {
-            hasNext: true,
-            hasPrevious: true,
-            gameCards: [],
-        },
-    ];
-
     const testGame: Game = {
         _id: '1',
         name: 'test',
         isHard: true,
-        original: 'test',
-        modified: 'test',
-        differencesCount: 1,
+        originalImage: 'test',
+        modifiedImage: 'test',
+        nDifference: 1,
         differences: 'test',
     };
-    const testGames: ServerSideGame[] = [
-        {
-            id: '',
-            name: 'test',
-            isHard: true,
-            original: 'test',
-            modified: 'test',
-            differencesCount: 1,
-            differences: [[]],
-        },
-    ];
     const testGameCard: GameCard = {
-        id: '1',
+        _id: '1',
         name: 'test',
         difficultyLevel: true,
         soloTopTime: defaultBestTimes,
         oneVsOneTopTime: defaultBestTimes,
-        thumbnail: 'test',
+        thumbnail: '',
     };
-    const testGameDto: CreateGameDto = {
-        name: 'test',
-        originalImage: 'test',
-        modifiedImage: 'test',
-        nDifference: 1,
-        differences: [[]],
-        isHard: true,
-    };
+    const testCarousel: CarouselPaginator[] = [
+        {
+            hasNext: true,
+            hasPrevious: true,
+            gameCards: [testGameCard],
+        },
+    ];
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -70,28 +49,27 @@ describe('GameListsManagerService', () => {
         expect(service).toBeDefined();
     });
 
-    it('buildGameCardFromGame() should call buildGameCarrousel()', () => {
+    it('addGameCarousel() should call readFileSync()', () => {
+        service['carouselGames'] = testCarousel;
         const readFileSyncSpy = jest.spyOn(fs, 'readFileSync').mockReturnValue('test');
-        const testGameCardFromGame = service.buildGameCardFromGame(testGame);
+        service.addGameCarousel(testGameCard);
         expect(readFileSyncSpy).toBeCalled();
-        expect(testGameCardFromGame).toEqual(testGameCard);
+        expect(service['carouselGames']).toEqual(testCarousel);
     });
-    it('getGamesCarrousel() should return the carrousel as expected', () => {
-        const testCarousel2: CarouselPaginator[] = [
-            {
-                hasNext: true,
-                hasPrevious: true,
-                gameCards: [testGameCard],
-            },
-        ];
-        service.addGameCarousel(testGameCard, testCarousel);
-        expect(testCarousel).toEqual(testCarousel2);
-    });
-    it('createGameFromGameDto() should create the game from the game dto', () => {
-        expect(service.createGameFromGameDto(testGameDto)).toEqual(testGames[0]);
+
+    it('buildGameCardFromGame() should create the game from the game dto', () => {
+        const testGameCard1: GameCard = {
+            _id: '1',
+            name: 'test',
+            difficultyLevel: true,
+            soloTopTime: defaultBestTimes,
+            oneVsOneTopTime: defaultBestTimes,
+            thumbnail: 'assets/test/original.bmp',
+        };
+        const gameCard = service.buildGameCardFromGame(testGame);
+        expect(gameCard).toEqual(testGameCard1);
     });
     it('buildGameCarrousel() should build the game carrousel when it is empty', () => {
-        const testCarousel2: CarouselPaginator[] = [];
         const testCarouselExpected: CarouselPaginator[] = [
             {
                 hasNext: true,
@@ -106,10 +84,11 @@ describe('GameListsManagerService', () => {
         ];
         const testGameCards: GameCard[] = [testGameCard, testGameCard, testGameCard, testGameCard, testGameCard, testGameCard];
         const addGameCarouselSpy = jest.spyOn(service, 'addGameCarousel');
-        service.buildGameCarousel(testGameCards, testCarousel2);
+        service.buildGameCarousel(testGameCards);
         expect(addGameCarouselSpy).toBeCalled();
-        expect(testCarouselExpected).toEqual(testCarousel2);
+        expect(service['carouselGames']).toEqual(testCarouselExpected);
     });
+
     it('buildGameCarrousel() should not build the game carrousel when it is filled with at least one carousel paginator', () => {
         const testCarousel2: CarouselPaginator[] = [
             {
@@ -120,7 +99,7 @@ describe('GameListsManagerService', () => {
         ];
         const testCarouselBefore: CarouselPaginator[] = testCarousel2;
         const addGameCarouselSpy = jest.spyOn(service, 'addGameCarousel');
-        service.buildGameCarousel([], testCarousel2);
+        service.buildGameCarousel([]);
         expect(addGameCarouselSpy).not.toBeCalled();
         expect(testCarouselBefore).toEqual(testCarousel2);
     });
