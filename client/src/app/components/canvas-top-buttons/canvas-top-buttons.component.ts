9a4cb0b7ca81c293ba7d5794d3d639ad620d28b9
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-03-01 22:40:16 -0500

    feat: created canvas-top-buttons component

---
+++ client/src/app/components/canvas-top-buttons/canvas-top-buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas-top-buttons',
+  templateUrl: './canvas-top-buttons.component.html',
+  styleUrls: ['./canvas-top-buttons.component.scss']
+})
+export class CanvasTopButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
