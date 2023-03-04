2af480c3a07772066830575e806ad45ef6480836
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-03-04 18:12:15 -0500

    style: linted client

--- client/src/app/components/canvas-dropdown-menu/canvas-dropdown-menu.component.spec.ts
+++ client/src/app/components/canvas-dropdown-menu/canvas-dropdown-menu.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CanvasDropdownMenuComponent } from './canvas-dropdown-menu.component';
 
 describe('CanvasDropdownMenuComponent', () => {
-  let component: CanvasDropdownMenuComponent;
-  let fixture: ComponentFixture<CanvasDropdownMenuComponent>;
+    let component: CanvasDropdownMenuComponent;
+    let fixture: ComponentFixture<CanvasDropdownMenuComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CanvasDropdownMenuComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [CanvasDropdownMenuComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(CanvasDropdownMenuComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(CanvasDropdownMenuComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
