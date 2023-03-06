bb139c6810659c38d6eda7ad425f68fb70ef4fb4
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2023-03-05 22:14:13 -0500

    feat: adding preview selection-Game maining design stuff

--- client/src/app/components/nav-bar/nav-bar.component.spec.ts
+++ client/src/app/components/nav-bar/nav-bar.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavBarComponent } from './nav-bar.component';
 
 describe('NavBarComponent', () => {
-  let component: NavBarComponent;
-  let fixture: ComponentFixture<NavBarComponent>;
+    let component: NavBarComponent;
+    let fixture: ComponentFixture<NavBarComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NavBarComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [NavBarComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(NavBarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(NavBarComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
