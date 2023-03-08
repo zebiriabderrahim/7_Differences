3d6431cc2c4e58355e41bdb5bbcf8f90baa425ea
ABDERRAHIM ZEBIRI <abderrahimzebiri@ABDERRAHIMs-MacBook-Pro.local>
2023-03-08 14:55:04 -0500

    feature: add joined player dialog component and update app module

---
+++ client/src/app/components/joined-player-dialog/joined-player-dialog.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { JoinedPlayerDialogComponent } from './joined-player-dialog.component';
+
+describe('JoinedPlayerDialogComponent', () => {
+  let component: JoinedPlayerDialogComponent;
+  let fixture: ComponentFixture<JoinedPlayerDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ JoinedPlayerDialogComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(JoinedPlayerDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
