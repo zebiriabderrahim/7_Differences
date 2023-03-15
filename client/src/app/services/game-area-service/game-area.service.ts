44aa3517689d28a5da0a26c00446c8c0cff44dac
JeremyEar <jeremy_ear@hotmail.com>
2023-03-15 17:00:34 -0400

    fix: added color names for clarity in flashCorrectPixels

--- client/src/app/services/game-area-service/game-area.service.ts
+++ client/src/app/services/game-area-service/game-area.service.ts
@@ -1,17 +1,7 @@
 import { HostListener, Injectable } from '@angular/core';
-import {
-    FLASH_WAIT_TIME,
-    GREEN_FLASH_TIME,
-    LEFT_BUTTON,
-    MAX_PIXEL_INTENSITY,
-    MIN_PIXEL_INTENSITY,
-    ONE_SECOND,
-    STRONG_PIXEL_INTENSITY,
-    X_CENTERING_DISTANCE,
-    YELLOW_FLASH_TIME,
-} from '@app/constants/constants';
+import { FLASH_WAIT_TIME, GREEN_FLASH_TIME, LEFT_BUTTON, ONE_SECOND, X_CENTERING_DISTANCE, YELLOW_FLASH_TIME } from '@app/constants/constants';
 import { IMG_HEIGHT, IMG_WIDTH } from '@app/constants/image';
-import { N_PIXEL_ATTRIBUTE } from '@app/constants/pixels';
+import { GREEN_PIXEL, N_PIXEL_ATTRIBUTE, YELLOW_PIXEL } from '@app/constants/pixels';
 import { Coordinate } from '@common/coordinate';
 
 @Injectable({
@@ -79,53 +69,52 @@ export class GameAreaService {
 
     flashCorrectPixels(differenceCoord: Coordinate[]): void {
         const imageDataIndexes = this.convert2DCoordToPixelIndex(differenceCoord);
-        this.flashPixelsLogic(imageDataIndexes);
+        this.flashPixels(imageDataIndexes);
     }
 
-    flashPixelsLogic(imageDataIndexes: number[]): void {
+    flashPixels(imageDataIndexes: number[]): void {
         const firstInterval = setInterval(() => {
             const secondInterval = setInterval(() => {
                 this.setPixelData(imageDataIndexes, this.modifiedFrontPixelData, this.originalFrontPixelData);
                 this.putImageDataToContexts();
             }, GREEN_FLASH_TIME);
 
-            const color = [MAX_PIXEL_INTENSITY, STRONG_PIXEL_INTENSITY, 0, MAX_PIXEL_INTENSITY];
+            const color = [YELLOW_PIXEL.red, YELLOW_PIXEL.green, YELLOW_PIXEL.blue, YELLOW_PIXEL.alpha];
             for (const index of imageDataIndexes) {
                 this.modifiedFrontPixelData.data.set(color, index);
                 this.originalFrontPixelData.data.set(color, index);
             }
             this.putImageDataToContexts();
 
-            this.setTime(secondInterval);
+            setTimeout(() => {
+                clearInterval(secondInterval);
+                this.clearFlashing();
+            }, FLASH_WAIT_TIME);
         }, YELLOW_FLASH_TIME);
 
-        this.setTime(firstInterval);
+        setTimeout(() => {
+            clearInterval(firstInterval);
+            this.clearFlashing();
+        }, FLASH_WAIT_TIME);
     }
 
     setPixelData(imageDataIndexes: number[], modifiedFrontPixelData: ImageData, originalFrontPixelData: ImageData): void {
         for (const index of imageDataIndexes) {
-            modifiedFrontPixelData.data[index] = MIN_PIXEL_INTENSITY;
-            modifiedFrontPixelData.data[index + 1] = MAX_PIXEL_INTENSITY;
-            modifiedFrontPixelData.data[index + 2] = MIN_PIXEL_INTENSITY;
-            modifiedFrontPixelData.data[index + 3] = MAX_PIXEL_INTENSITY;
+            modifiedFrontPixelData.data[index] = GREEN_PIXEL.red;
+            modifiedFrontPixelData.data[index + 1] = GREEN_PIXEL.green;
+            modifiedFrontPixelData.data[index + 2] = GREEN_PIXEL.blue;
+            modifiedFrontPixelData.data[index + 3] = GREEN_PIXEL.alpha;
 
-            originalFrontPixelData.data[index] = MIN_PIXEL_INTENSITY;
-            originalFrontPixelData.data[index + 1] = MAX_PIXEL_INTENSITY;
-            originalFrontPixelData.data[index + 2] = MIN_PIXEL_INTENSITY;
-            originalFrontPixelData.data[index + 3] = MAX_PIXEL_INTENSITY;
+            originalFrontPixelData.data[index] = GREEN_PIXEL.red;
+            originalFrontPixelData.data[index + 1] = GREEN_PIXEL.green;
+            originalFrontPixelData.data[index + 2] = GREEN_PIXEL.blue;
+            originalFrontPixelData.data[index + 3] = GREEN_PIXEL.alpha;
         }
     }
 
     putImageDataToContexts(): void {
         this.modifiedContextFrontLayer.putImageData(this.modifiedFrontPixelData, 0, 0);
         this.originalContextFrontLayer.putImageData(this.originalFrontPixelData, 0, 0);
-    }
-
-    setTime(intervalNumber: NodeJS.Timer): void {
-        setTimeout(() => {
-            clearInterval(intervalNumber);
-            this.clearFlashing();
-        }, FLASH_WAIT_TIME);
     }
 
     clearFlashing(): void {
