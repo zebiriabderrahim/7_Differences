2571ff4c4fa55629d99f7271f258a275de28461f
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-03-16 00:29:26 -0400

    test: foreground-service almost done

--- client/src/app/services/draw-service/draw.service.spec.ts
+++ client/src/app/services/draw-service/draw.service.spec.ts
@@ -1,3 +1,5 @@
+// Needed to test private methods
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { TestBed } from '@angular/core/testing';
 import { CanvasTestHelper } from '@app/classes/canvas-test-helper';
 import { IMG_HEIGHT, IMG_WIDTH } from '@app/constants/image';
@@ -81,7 +83,7 @@ describe('DrawService', () => {
 
     it('isCurrentActionRectangle should return true if currentAction is rectangle', () => {
         service['currentAction'] = CanvasAction.Rectangle;
-        expect(service.isCurrentActionRectangle()).toBeTrue();
+        expect(service.isCurrentActionRectangle()).toBeTruthy();
     });
 
     it('isCurrentActionRectangle should return false if currentAction is not rectangle', () => {
@@ -89,24 +91,131 @@ describe('DrawService', () => {
         expect(service.isCurrentActionRectangle()).toBeFalse();
     });
 
-    // TODO : Mouse being dragged test
-    // it('isMouseBeingDragged should return true if mouse is being dragged', () => {
-    //     service['isMouseBeingDragged'] = true;
-    //     expect(service.isMouseBeingDragged()).toBeTrue();
-    // }
+    it('isMouseDragged should return true if mouse is being dragged', () => {
+        service['isMouseBeingDragged'] = true;
+        expect(service.isMouseDragged()).toBeTruthy();
+    });
 
-    // TODO : isOperationValid test
+    it('isOperationValid should return true if mouse is being dragged and mouse is not out of canvas', () => {
+        service['isMouseBeingDragged'] = true;
+        service['activeCanvasPosition'] = CanvasPosition.Left;
+        service.isOperationValid(CanvasPosition.Left);
+        expect(service.isOperationValid(CanvasPosition.Left)).toBeTruthy();
+    });
+
+    it('isOperationValid should return false if mouse is not being dragged', () => {
+        service['isMouseBeingDragged'] = false;
+        expect(service.isOperationValid(CanvasPosition.Left)).toBeFalsy();
+    });
+
+    it('isOperationValid should return false if mouse is out of canvas', () => {
+        service['isMouseBeingDragged'] = true;
+        service['activeCanvasPosition'] = CanvasPosition.Left;
+        expect(service.isOperationValid(CanvasPosition.Right)).toBeFalsy();
+    });
 
     it('disableMouseDrag should set isMouseBeingDragged to false if its true', () => {
         service['isMouseBeingDragged'] = true;
         service.disableMouseDrag();
-        expect(service['isMouseBeingDragged']).toBeFalse();
+        expect(service['isMouseBeingDragged']).toBeFalsy();
     });
 
     it('disableMouseDrag should not change isMouseBeingDragged if its already false', () => {
         service['isMouseBeingDragged'] = false;
         service.disableMouseDrag();
-        expect(service['isMouseBeingDragged']).toBeFalse();
+        expect(service['isMouseBeingDragged']).toBeFalsy();
+    });
+
+    it('startOperation should set isMouseBeingDragged to true, call setCanvasOperationStyle', () => {
+        service['isMouseBeingDragged'] = false;
+        service['activeContext'] = contextStub;
+        // eslint-disable-next-line @typescript-eslint/no-empty-function -- needed for empty callFake
+        spyOn<any>(service, 'drawLine').and.callFake(() => {});
+        // eslint-disable-next-line @typescript-eslint/no-empty-function -- needed for empty callFake
+        const setCanvasOperationStyleSpy = spyOn<any>(service, 'setCanvasOperationStyle').and.callFake(() => {});
+        service.startOperation();
+        expect(service['isMouseBeingDragged']).toBeTruthy();
+        expect(setCanvasOperationStyleSpy).toHaveBeenCalled();
+    });
+
+    it('startOperation should call beginPath and drawLine if isCurrentActionRectangle is false', () => {
+        service['activeContext'] = contextStub;
+        const beginPathSpy = spyOn(contextStub, 'beginPath');
+        spyOn(service, 'isCurrentActionRectangle').and.callFake(() => false);
+
+        // eslint-disable-next-line @typescript-eslint/no-empty-function -- needed for empty callFake
+        const drawLineSpy = spyOn<any>(service, 'drawLine').and.callFake(() => {});
+        // eslint-disable-next-line @typescript-eslint/no-empty-function -- needed for empty callFake
+        spyOn<any>(service, 'setCanvasOperationStyle').and.callFake(() => {});
+
+        service.startOperation();
+        expect(drawLineSpy).toHaveBeenCalled();
+        expect(beginPathSpy).toHaveBeenCalled();
+    });
+
+    it('startOperation should should set recTangleTopCorner to click and drawLine is isCurrentActionRectangle is true', () => {
+        const fakeCoordinate: Coordinate = { x: 33, y: 33 };
+        service['clickPosition'] = fakeCoordinate;
+        service['activeContext'] = contextStub;
+        spyOn(service, 'isCurrentActionRectangle').and.callFake(() => true);
+
+        // eslint-disable-next-line @typescript-eslint/no-empty-function -- needed for empty callFake
+        spyOn<any>(service, 'setCanvasOperationStyle').and.callFake(() => {});
+        expect(service['rectangleTopCorner']).not.toBe(service['clickPosition']);
+        service.startOperation();
+        expect(service['rectangleTopCorner']).toBe(service['clickPosition']);
+    });
+
+    // TODO: ContinueCanvasOperation
+
+    it('stopOperation should call disableMouseDrag and drawLine if isCurrentActionRectangle is false', () => {
+        spyOn(service, 'isCurrentActionRectangle').and.callFake(() => false);
+        // eslint-disable-next-line @typescript-eslint/no-empty-function -- needed for empty callFake
+        const drawLineSpy = spyOn<any>(service, 'drawLine').and.callFake(() => {});
+        const disableMouseDragSpy = spyOn<any>(service, 'disableMouseDrag');
+        service.stopOperation();
+        expect(drawLineSpy).toHaveBeenCalled();
+        expect(disableMouseDragSpy).toHaveBeenCalled();
+    });
+
+    it('stopOperation should call disableMouseDrag and drawRectangle if isCurrentActionRectangle is true', () => {
+        spyOn(service, 'isCurrentActionRectangle').and.callFake(() => true);
+        // eslint-disable-next-line @typescript-eslint/no-empty-function -- needed for empty callFake
+        const drawRectangleSpy = spyOn<any>(service, 'drawRectangle').and.callFake(() => {});
+        const disableMouseDragSpy = spyOn<any>(service, 'disableMouseDrag');
+        service.stopOperation();
+        expect(drawRectangleSpy).toHaveBeenCalled();
+        expect(disableMouseDragSpy).toHaveBeenCalled();
+    });
+
+    it('setSquareMode should not set isSquareModeOn to value if mouse is not dragged', () => {
+        const mockSquareMode = true;
+        service['isMouseBeingDragged'] = false;
+        expect(service['isSquareModeOn']).not.toBe(mockSquareMode);
+        service.setSquareMode(mockSquareMode);
+        expect(service['isSquareModeOn']).not.toBe(mockSquareMode);
+    });
+
+    it('setSquareMode should not set isSquareModeOn to value if not in rectangle action', () => {
+        const mockSquareMode = true;
+        service['isMouseBeingDragged'] = true;
+        spyOn(service, 'isCurrentActionRectangle').and.callFake(() => false);
+        expect(service['isSquareModeOn']).not.toBe(mockSquareMode);
+        service.setSquareMode(mockSquareMode);
+        expect(service['isSquareModeOn']).not.toBe(mockSquareMode);
+    });
+
+    it('setSquareMode should call drawRectangle and set the squareMode if mouse is dragged in rectangle', () => {
+        const mockSquareMode = true;
+        service['isMouseBeingDragged'] = true;
+        spyOn(service, 'isCurrentActionRectangle').and.callFake(() => true);
+
+        // eslint-disable-next-line @typescript-eslint/no-empty-function -- needed for empty callFake
+        const drawRectangleSpy = spyOn<any>(service, 'drawRectangle').and.callFake(() => {});
+        expect(service['isSquareModeOn']).not.toBe(mockSquareMode);
+        service.setSquareMode(mockSquareMode);
+        expect(drawRectangleSpy).toHaveBeenCalled();
+        expect(service['isSquareModeOn']).toBe(mockSquareMode);
     });
 
     it('drawRectangle should call clearRect and fillRect on activeContext', () => {
