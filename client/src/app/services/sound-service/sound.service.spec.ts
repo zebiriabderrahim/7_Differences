2997eb03e54919ddea1028169ab0a5d217615873
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-03-17 00:31:43 -0400

    style: linted all files

--- client/src/app/services/sound-service/sound.service.spec.ts
+++ client/src/app/services/sound-service/sound.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { SoundService } from './sound.service';
 
 describe('SoundService', () => {
-  let service: SoundService;
+    let service: SoundService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SoundService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(SoundService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
