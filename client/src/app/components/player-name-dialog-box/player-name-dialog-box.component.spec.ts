c92060e784b81023757b67c31ecd60ffc1f4aaa1
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-03-16 20:18:46 -0400

    test: validatePlayerName done

--- client/src/app/components/player-name-dialog-box/player-name-dialog-box.component.spec.ts
+++ client/src/app/components/player-name-dialog-box/player-name-dialog-box.component.spec.ts
@@ -9,12 +9,10 @@ describe('PlayerNameDialogBoxComponent', () => {
     let component: PlayerNameDialogBoxComponent;
     let fixture: ComponentFixture<PlayerNameDialogBoxComponent>;
     let dialogRef: MatDialogRef<PlayerNameDialogBoxComponent, unknown>;
-    let roomManagerService: RoomManagerService;
-    let control: FormControl;
     let roomManagerServiceSpy: jasmine.SpyObj<RoomManagerService>;
 
     beforeEach(async () => {
-        const mockIsNameTaken = new BehaviorSubject({ gameId: component['data'].gameId, isNameAvailable: true });
+        const mockIsNameTaken = new BehaviorSubject({ gameId: '1', isNameAvailable: false });
         roomManagerServiceSpy = jasmine.createSpyObj('RoomManagerService', ['isPlayerNameIsAlreadyTaken', 'isNameTaken$'], {
             isNameTaken$: mockIsNameTaken,
         });
@@ -23,14 +21,13 @@ describe('PlayerNameDialogBoxComponent', () => {
             declarations: [],
             providers: [
                 { provide: MatDialogRef, useValue: { close: jasmine.createSpy('close') } },
-                { provide: MAT_DIALOG_DATA, useValue: { id: 1 } },
+                { provide: MAT_DIALOG_DATA, useValue: { id: '1' } },
                 { provide: RoomManagerService, useValue: roomManagerServiceSpy },
             ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(PlayerNameDialogBoxComponent);
         component = fixture.componentInstance;
-        // roomManagerService = TestBed.inject(RoomManagerService);
         dialogRef = TestBed.inject(MatDialogRef<PlayerNameDialogBoxComponent, unknown>);
     });
 
@@ -56,26 +53,19 @@ describe('PlayerNameDialogBoxComponent', () => {
     });
 
     it('validatePlayerName should return null if player name is not taken', async () => {
-        control.setValue('testName');
+        const control = jasmine.createSpyObj('AbstractControl', ['value']);
+        control.value = 'ExistentGameName';
         const result = await component.validatePlayerName(control);
 
-        expect(roomManagerService.isPlayerNameIsAlreadyTaken).toHaveBeenCalledWith(component['data'].gameId, 'testName');
-        expect(roomManagerService.isNameTaken$).toHaveBeenCalled();
         expect(result).toBeNull();
     });
 
-    // it('validatePlayerName should return { nameTaken: true } if player name is taken', async () => {
-    //     roomManagerServiceSpy.isNameTaken$ = new BehaviorSubject({ gameId: component['data'].gameId, isNameAvailable: false });
-    //     const isNameTaken$ = new BehaviorSubject({ gameId: component['data'].gameId, isNameAvailable: false });
-    //     roomManagerServiceSpy.isNameTaken$.subscribe((value) => {
-    //         console.log(value);
-    //     });
+    it('validatePlayerName should return { nameTaken: true } if player name is taken', async () => {
+        component['data'] = { gameId: '1' };
+        const control = jasmine.createSpyObj('AbstractControl', ['value']);
+        control.value = 'ExistentGameName';
+        const result = await component.validatePlayerName(control);
 
-    //     control.setValue('testName');
-    //     const result = await component.validatePlayerName(control);
-
-    //     expect(roomManagerService.isPlayerNameIsAlreadyTaken).toHaveBeenCalledWith(component['data'].gameId, 'testName');
-    //     expect(roomManagerService.isNameTaken$).toHaveBeenCalled();
-    //     expect(result).toEqual({ nameTaken: true });
-    // });
+        expect(result).toEqual({ nameTaken: true });
+    });
 });
