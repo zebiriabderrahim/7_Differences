3b2c9594f17a18da730a35921e36678da998bbae
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2023-03-17 18:05:17 -0400

    Feat: test of one Fucking pipe done

--- client/src/app/components/joined-player-dialog/joined-player-dialog.component.spec.ts
+++ client/src/app/components/joined-player-dialog/joined-player-dialog.component.spec.ts
@@ -3,31 +3,36 @@ import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/materia
 import { RouterTestingModule } from '@angular/router/testing';
 import { RoomManagerService } from '@app/services/room-manager-service/room-manager.service';
 import { WaitingPlayerNameList } from '@common/game-interfaces';
-import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { JoinedPlayerDialogComponent } from './joined-player-dialog.component';
 
 describe('JoinedPlayerDialogComponent', () => {
     let component: JoinedPlayerDialogComponent;
     let fixture: ComponentFixture<JoinedPlayerDialogComponent>;
     let roomManagerServiceSpy: jasmine.SpyObj<RoomManagerService>;
-    let joinedPlayerNamesMock: Subject<WaitingPlayerNameList>;
+    let joinedPlayerNamesMock: BehaviorSubject<WaitingPlayerNameList>;
+    let dialogRefSpy: jasmine.SpyObj<MatDialogRef<JoinedPlayerDialogComponent>>;
 
     beforeEach(async () => {
-        joinedPlayerNamesMock = new Subject<WaitingPlayerNameList>();
-        roomManagerServiceSpy = jasmine.createSpyObj('RoomManagerService', [], { joinedPlayerNamesByGameId$: joinedPlayerNamesMock.asObservable() });
+        joinedPlayerNamesMock = new BehaviorSubject<WaitingPlayerNameList>({
+            gameId: 'test-game-id',
+            playerNamesList: ['Alice', 'Bob'],
+        });
+        dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close', 'afterClosed']);
+        roomManagerServiceSpy = jasmine.createSpyObj('RoomManagerService', ['cancelJoining'], {
+            joinedPlayerNamesByGameId$: joinedPlayerNamesMock,
+        });
         await TestBed.configureTestingModule({
             declarations: [JoinedPlayerDialogComponent],
             imports: [MatDialogModule, RouterTestingModule],
             providers: [
-                { provide: MAT_DIALOG_DATA, useValue: { gameId: 'game123', player: 'testPlayer' } },
-                // eslint-disable-next-line @typescript-eslint/no-empty-function
-                { provide: MatDialogRef, useValue: { close: () => {} } },
+                { provide: MAT_DIALOG_DATA, useValue: { gameId: 'test-game-id', player: 'Alice' } },
                 { provide: RoomManagerService, useValue: roomManagerServiceSpy },
+                { provide: MatDialogRef, useValue: dialogRefSpy },
             ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(JoinedPlayerDialogComponent);
-        // roomManagerService = TestBed.inject(RoomManagerService);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
@@ -39,88 +44,34 @@ describe('JoinedPlayerDialogComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    // it('should handle refused player and accepted player', () => {
-    //     const mockData = { gameId: 'game123', playerNamesList: ['player1', 'player2'] };
-    //     // expect(component['data']).toBeNull();
-    //     const spyPipe = spyOn(roomManagerServiceSpy.joinedPlayerNamesByGameId$, 'pipe').and.returnValue(joinedPlayerNamesMock);
-    //     component.getJoinedPlayerNamesByGameId();
-    //     // joinedPlayerNamesMock.next(mockData);
-    //     // joinedPlayerNamesMock.next(mockData);
+    it('should call roomManagerService.cancelJoining with correct arguments', () => {
+        component.cancelJoining();
+        expect(roomManagerServiceSpy.cancelJoining).toHaveBeenCalledWith('test-game-id', 'Alice');
+    });
 
-    //     spyOn(component, 'handleRefusedPlayer');
-    //     spyOn(component, 'handleAcceptedPlayer');
-    //     expect(spyPipe).toHaveBeenCalled();
-    //     expect(component.handleRefusedPlayer).toHaveBeenCalledWith(mockData.playerNamesList);
-    //     expect(component.handleAcceptedPlayer).toHaveBeenCalled();
-    // });
+    it('should handle refused and accepted players when player names are received', () => {
+        spyOn(component, 'handleRefusedPlayer');
+        spyOn(component, 'handleAcceptedPlayer');
 
-    // it('should filter player names by game id', () => {
-    // arrange
-    // const gameId = 'game1';
-    // const player = 'player1';
-    // const joinedPlayerNamesByGameId$ = new BehaviorSubject({
-    //     gameId,
-    //     playerNamesList: ['player1', 'player2'],
-    // });
-    // const predicateFn = (data: any) => data.gameId === 'myGameId' && !!data.playerNamesList;
-    // spyOn(filter, 'filter')
-    // const filterSpy = spyOn(rxjs, 'filter').and.callFake((predicate: (value: any, index: number) => boolean) => {
-    //     return filter((data) => predicateFn(data));
-    // });
-    // component['data'] = { gameId, player };
+        joinedPlayerNamesMock.next({
+            gameId: 'test-game-id',
+            playerNamesList: ['Alice', 'Bob', 'Charlie'],
+        });
 
-    // // act
-    // component.getJoinedPlayerNamesByGameId();
-    // joinedPlayerNamesByGameId$.next({
-    //     gameId,
-    //     playerNamesList: ['player1', 'player2'],
+        expect(component.handleRefusedPlayer).toHaveBeenCalledWith(['Alice', 'Bob', 'Charlie']);
+        expect(component.handleAcceptedPlayer).toHaveBeenCalled();
+    });
 
-    // assert
-    // expect(filterSpy).toHaveBeenCalledWith(
-    //     jasmine.any(Function), // This checks if the filter function is passed in as an argument
-    // );
-    //     const gameId = '123';
-    //     component['data'] = { gameId, player: 'player1' };
-    //     const playerNamesList = ['player1', 'player2'];
-
-    //     // Spy on roomManagerService
-    //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    //     const joinedPlayerNamesByGameIdSpy = spyOn(roomManagerService, 'joinedPlayerNamesByGameId$' as any).and.returnValue(
-    //         of({ gameId, playerNamesList }),
-    //     );
-
-    //     // Call method
-    //     component.getJoinedPlayerNamesByGameId();
-
-    //     // Expectations
-    //     expect(joinedPlayerNamesByGameIdSpy).toHaveBeenCalled();
-    //     expect(joinedPlayerNamesByGameIdSpy).toHaveBeenCalledTimes(1);
-
-    //     // Wait for observable to emit before testing filter
-    //     joinedPlayerNamesByGameIdSpy.and.returnValue(of({ gameId, playerNamesList }).pipe(delay(0)));
-    //     fixture.detectChanges();
-
-    //     expect(component.getJoinedPlayerNamesByGameId).toBeDefined();
-
-    //     const filterSpy = spyOn('filter').and.callThrough();
-    //     const expectedParameter = { gameId, playerNamesList };
-    //     const expectedReturnValue = gameId === component['data'].gameId && !!playerNamesList;
-
-    //     expect(filterSpy).toHaveBeenCalledWith(expectedParameter);
-    //     expect(filterSpy).toHaveBeenCalledTimes(1);
-    // });
-
-    // it('should handle refused player and accepted player', () => {
-    //     const mockData = { gameId: 'game123', playerNamesList: ['player1', 'player2'] };
-    //     // expect(component['data']).toBeNull();
-    //     const spyPipe = spyOn(roomManagerService.joinedPlayerNamesByGameId$, 'pipe').and.returnValue(
-    //         of(mockData).pipe(filter((data) => data.gameId === 'game123' && !!data.playerNamesList)),
-    //     );
-    //     component.getJoinedPlayerNamesByGameId();
-    //     spyOn(component, 'handleRefusedPlayer');
-    //     spyOn(component, 'handleAcceptedPlayer');
-    //     expect(spyPipe).toHaveBeenCalled();
-    //     expect(component.handleRefusedPlayer).toHaveBeenCalledWith(mockData.playerNamesList);
-    //     expect(component.handleAcceptedPlayer).toHaveBeenCalled();
-    // });
+    it('should start countdown and show message if player is not in playerNames', () => {
+        // component['data'] = { gameId: 'game123', player: 'testPlayer' };
+        // const playerNames = ['Alice', 'Charlie'];
+        // component.handleRefusedPlayer(playerNames);
+        // expect(component.countdown).toBe(10);
+        // expect(component.refusedMessage).toBe(`You have been refused. You will be redirected in ${component.countdown} seconds`);
+        // // Simulate countdown
+        // // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        // jasmine.clock().install();
+        // jasmine.clock().tick(10000);
+        // expect(dialogRefSpy.close).toHaveBeenCalled();
+    });
 });
