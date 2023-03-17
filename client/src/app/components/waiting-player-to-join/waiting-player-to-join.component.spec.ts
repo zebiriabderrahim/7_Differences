0741710bae6f7bf8d11332c55145eee2e2de7240
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-03-17 13:57:53 -0400

    test: refusePlayer done

--- client/src/app/components/waiting-player-to-join/waiting-player-to-join.component.spec.ts
+++ client/src/app/components/waiting-player-to-join/waiting-player-to-join.component.spec.ts
@@ -1,13 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ClientSocketService } from '@app/services/client-socket-service/client-socket.service';
+import { RoomManagerService } from '@app/services/room-manager-service/room-manager.service';
 import { WaitingForPlayerToJoinComponent } from './waiting-player-to-join.component';
 
 describe('WaitingPlayerToJoinComponent', () => {
     let component: WaitingForPlayerToJoinComponent;
     let fixture: ComponentFixture<WaitingForPlayerToJoinComponent>;
+    let roomManagerService: RoomManagerService;
+    let clientSocketService: ClientSocketService;
 
     beforeEach(async () => {
+        // roomManagerServiceSpy = jasmine.createSpyObj('RoomManagerService', ['refusePlayer']);
+        // clientSocketServiceSpy = jasmine.createSpyObj('ClientSocketService', ['send']);
         await TestBed.configureTestingModule({
             declarations: [WaitingForPlayerToJoinComponent],
             imports: [MatDialogModule, RouterTestingModule],
@@ -16,7 +22,8 @@ describe('WaitingPlayerToJoinComponent', () => {
                 { provide: MatDialogRef, useValue: {} },
             ],
         }).compileComponents();
-
+        roomManagerService = TestBed.inject(RoomManagerService);
+        clientSocketService = TestBed.inject(ClientSocketService);
         fixture = TestBed.createComponent(WaitingForPlayerToJoinComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -24,5 +31,15 @@ describe('WaitingPlayerToJoinComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('refusePlayer should refuse the player using the roomManagerService', () => {
+        const gameId = '12';
+        const playerName = 'John';
+        component['data'] = { roomId: '23', player: playerName, gameId };
+        spyOn(roomManagerService, 'refusePlayer');
+        spyOn(clientSocketService, 'send');
+        component.refusePlayer('John');
+        expect(roomManagerService.refusePlayer).toHaveBeenCalledWith(gameId, playerName);
     });
 });
