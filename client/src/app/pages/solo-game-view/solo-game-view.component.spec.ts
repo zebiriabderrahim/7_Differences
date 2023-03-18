d4dd5c11b13e94ca9f0d4e4952ec3bd8a5fd08b9
ZakariaZair <zakaria.zair@polymtl.ca>
2023-03-18 17:38:08 -0400

    test: added message manager tests

--- client/src/app/pages/solo-game-view/solo-game-view.component.spec.ts
+++ client/src/app/pages/solo-game-view/solo-game-view.component.spec.ts
@@ -170,9 +170,12 @@ describe('SoloGameViewComponent', () => {
         expect(dialog.open).toHaveBeenCalled();
     });
 
-    // it('addRightSideMessage should add a message to the messages array and call ClassicService.sendMessage', () => {
-    //     const text = 'Bravo !';
-    //     component.addRightSideMessage(text);
-    //     expect(classicServiceSpy.sendMessage).toHaveBeenCalledWith(text);
-    // });
+    it('addRightSideMessage should add a message to the messages array and call ClassicService.sendMessage', () => {
+        const sendMessageSpy = spyOn(component['classicService'], 'sendMessage');
+        const text = 'Bravo !';
+        const messagesLength = component.messages.length;
+        component.addRightSideMessage(text);
+        expect(messagesLength).toBeLessThan(component.messages.length);
+        expect(sendMessageSpy).toHaveBeenCalledWith(text);
+    });
 });
