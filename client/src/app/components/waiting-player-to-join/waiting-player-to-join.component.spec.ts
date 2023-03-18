8abeb1ffb2da645e66bcb46abc44921b8b8b3851
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-03-17 22:02:18 -0400

    test: getJoinedPlayerNamesByGameId done

--- client/src/app/components/waiting-player-to-join/waiting-player-to-join.component.spec.ts
+++ client/src/app/components/waiting-player-to-join/waiting-player-to-join.component.spec.ts
@@ -4,18 +4,27 @@ import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ClientSocketService } from '@app/services/client-socket-service/client-socket.service';
 import { RoomManagerService } from '@app/services/room-manager-service/room-manager.service';
-import { map, of } from 'rxjs';
+import { WaitingPlayerNameList } from '@common/game-interfaces';
+import { BehaviorSubject, map, of } from 'rxjs';
 import { WaitingForPlayerToJoinComponent } from './waiting-player-to-join.component';
 
 describe('WaitingPlayerToJoinComponent', () => {
     let component: WaitingForPlayerToJoinComponent;
     let fixture: ComponentFixture<WaitingForPlayerToJoinComponent>;
-    let roomManagerService: RoomManagerService;
+    let roomManagerServiceSpy: jasmine.SpyObj<RoomManagerService>;
     let clientSocketServiceSpy: jasmine.SpyObj<ClientSocketService>;
     let dialogRefSpy: jasmine.SpyObj<MatDialogRef<WaitingForPlayerToJoinComponent>>;
     let router: jasmine.SpyObj<Router>;
+    let joinedPlayerNamesMock: BehaviorSubject<WaitingPlayerNameList>;
 
     beforeEach(async () => {
+        joinedPlayerNamesMock = new BehaviorSubject<WaitingPlayerNameList>({
+            gameId: 'test-game-id',
+            playerNamesList: ['Alice', 'Bob'],
+        });
+        roomManagerServiceSpy = jasmine.createSpyObj('RoomManagerService', ['refusePlayer', 'acceptPlayer', 'deleteCreatedOneVsOneRoom'], {
+            joinedPlayerNamesByGameId$: joinedPlayerNamesMock,
+        });
         clientSocketServiceSpy = jasmine.createSpyObj('ClientSocketService', ['send', 'disconnect']);
         dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['afterClosed']);
         dialogRefSpy.afterClosed.and.returnValue(of('dialog closed'));
@@ -24,13 +33,13 @@ describe('WaitingPlayerToJoinComponent', () => {
             declarations: [WaitingForPlayerToJoinComponent],
             imports: [MatDialogModule, RouterTestingModule],
             providers: [
-                { provide: MAT_DIALOG_DATA, useValue: {} },
+                { provide: MAT_DIALOG_DATA, useValue: { gameId: 'test-game-id', player: 'Alice' } },
                 { provide: MatDialogRef, useValue: dialogRefSpy },
                 { provide: Router, useValue: router },
                 { provide: ClientSocketService, useValue: clientSocketServiceSpy },
+                { provide: RoomManagerService, useValue: roomManagerServiceSpy },
             ],
         }).compileComponents();
-        roomManagerService = TestBed.inject(RoomManagerService);
         fixture = TestBed.createComponent(WaitingForPlayerToJoinComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -40,17 +49,25 @@ describe('WaitingPlayerToJoinComponent', () => {
         expect(component).toBeTruthy();
     });
 
+    it('getJoinedPlayerNamesByGameId should subscribe to joinedPlayerNamesByGameId$ and set playerNames', () => {
+        joinedPlayerNamesMock.next({
+            gameId: 'test-game-id',
+            playerNamesList: ['Alice', 'Bob', 'Charlie'],
+        });
+
+        expect(component['playerNamesSubscription']).toBeDefined();
+        expect(component.playerNames).toEqual(['Alice', 'Bob', 'Charlie']);
+    });
+
     it('refusePlayer should refuse the player using the roomManagerService', () => {
         const gameId = '12';
         const playerName = 'John';
         component['data'] = { roomId: '23', player: playerName, gameId };
-        spyOn(roomManagerService, 'refusePlayer');
         component.refusePlayer('John');
-        expect(roomManagerService.refusePlayer).toHaveBeenCalledWith(gameId, playerName);
+        expect(roomManagerServiceSpy.refusePlayer).toHaveBeenCalledWith(gameId, playerName);
     });
 
     it('acceptPlayer should navigate to the game page after dialog close', () => {
-        spyOn(roomManagerService, 'acceptPlayer');
         // eslint-disable-next-line @typescript-eslint/no-empty-function -- needed for fake afterClosed
         dialogRefSpy.afterClosed.and.returnValue(of({}).pipe(map(() => {})));
         component.acceptPlayer('Alice');
@@ -58,14 +75,13 @@ describe('WaitingPlayerToJoinComponent', () => {
     });
 
     it('acceptPlayer should refuse all other players and accept the given player', () => {
-        const roomManagerServiceSpy = spyOn(roomManagerService, 'acceptPlayer');
         const refusePlayerSpy = spyOn(component, 'refusePlayer');
         component.playerNames = ['Alice', 'Bob', 'Charlie'];
         fixture.detectChanges();
         component.acceptPlayer('Alice');
         expect(refusePlayerSpy).toHaveBeenCalledWith('Bob');
         expect(refusePlayerSpy).toHaveBeenCalledWith('Charlie');
-        expect(roomManagerServiceSpy).toHaveBeenCalled();
+        expect(roomManagerServiceSpy.acceptPlayer).toHaveBeenCalled();
         expect(router.navigate).toHaveBeenCalledWith(['/game', undefined]);
     });
 
@@ -73,11 +89,10 @@ describe('WaitingPlayerToJoinComponent', () => {
         const gameId = '123';
         const playerNames = ['John', 'Jane'];
         component['data'] = { roomId: '23', player: playerNames[0], gameId };
-        spyOn(roomManagerService, 'deleteCreatedOneVsOneRoom');
         spyOn(component, 'refusePlayer');
         component.playerNames = playerNames;
         component.undoCreateOneVsOneRoom();
-        expect(roomManagerService.deleteCreatedOneVsOneRoom).toHaveBeenCalledWith(gameId);
+        expect(roomManagerServiceSpy.deleteCreatedOneVsOneRoom).toHaveBeenCalledWith(gameId);
         expect(component.refusePlayer).toHaveBeenCalledTimes(playerNames.length);
         playerNames.forEach((player) => {
             expect(component.refusePlayer).toHaveBeenCalledWith(player);
