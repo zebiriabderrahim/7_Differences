a449219dce7193c8db445636beba8ca6c64ccdb2
ZakariaZair <zakaria.zair@polymtl.ca>
2023-03-20 17:01:32 -0400

    fix: ec2 deploy

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://' + 'myServerURL' + ':3000/api',
+    serverUrl: 'http://' + 'myServerURL' + ':3000', //ec2 redploy client, redeploy server
 };
