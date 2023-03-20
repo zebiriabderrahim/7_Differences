a67bbda41c23277057cf5734d9ada31c823ffd4d
JeremyEar <jeremy_ear@hotmail.com>
2023-03-20 09:46:34 -0400

    fix: changed solo game view and solo game view dialog to generic names

--- client/src/app/components/game-page-dialog/game-page-dialog.component.ts
+++ client/src/app/components/game-page-dialog/game-page-dialog.component.ts
@@ -2,11 +2,11 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ClassicSystemService } from '@app/services/classic-system-service/classic-system.service';
 @Component({
-    selector: 'app-solo-game-view-dialog',
-    templateUrl: './solo-game-view-dialog.component.html',
-    styleUrls: ['./solo-game-view-dialog.component.scss'],
+    selector: 'app-game-page-dialog',
+    templateUrl: './game-page-dialog.component.html',
+    styleUrls: ['./game-page-dialog.component.scss'],
 })
-export class SoloGameViewDialogComponent {
+export class GamePageDialogComponent {
     constructor(@Inject(MAT_DIALOG_DATA) public data: { action: string; message: string }, public classicSystem: ClassicSystemService) {}
     abandonGame(): void {
         this.classicSystem.abandonGame();
