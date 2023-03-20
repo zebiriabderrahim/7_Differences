72372463f90cecfa7491f3281221012c356b1ebc
ZakariaZair <zakaria.zair@polymtl.ca>
2023-03-20 17:07:39 -0400

    fix: ec2 name

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://' + 'myServerURL' + ':3000', //ec2 redploy client, redeploy server
+    serverUrl: 'http://' + 'ec2-35-183-133-236.ca-central-1.compute.amazonaws.com' + ':3000', //ec2 redploy client, redeploy server
 };
