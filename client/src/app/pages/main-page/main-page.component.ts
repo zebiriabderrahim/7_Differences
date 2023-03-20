43031ee17943207d8e458feb9a91dea07db147d6
JeremyEar <jeremy_ear@hotmail.com>
2023-03-20 12:15:26 -0400

    fix: initialized main page properties in the constructor

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -6,17 +6,18 @@ import { Component } from '@angular/core';
     styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent {
-    readonly gameTitle: string = 'Rat-Coon Game';
-    readonly gameRoute: string = '/game';
-    readonly selectionRoute: string = '/selection';
-    readonly configRoute: string = '/config';
-    readonly teamNumber: string = "Présenté par l'équipe 101";
-    readonly teammateNameList: string[] = [
-        'Jeremy Ear,',
-        'Sulayman Hosna,',
-        'Edgar Kappauf,',
-        'Mathieu Prévost,',
-        'Zakaria Zair,',
-        'Abderrahim Zebiri',
-    ];
+    readonly gameTitle: string;
+    readonly gameRoute: string;
+    readonly selectionRoute: string;
+    readonly configRoute: string;
+    readonly teamNumber: string;
+    readonly teammateNameList: string[];
+    constructor() {
+        this.gameTitle = 'Rat-Coon Game';
+        this.gameRoute = '/game';
+        this.selectionRoute = '/selection';
+        this.configRoute = '/config';
+        this.teamNumber = "Présenté par l'équipe 101";
+        this.teammateNameList = ['Jeremy Ear,', 'Sulayman Hosna,', 'Edgar Kappauf,', 'Mathieu Prévost,', 'Zakaria Zair,', 'Abderrahim Zebiri'];
+    }
 }
