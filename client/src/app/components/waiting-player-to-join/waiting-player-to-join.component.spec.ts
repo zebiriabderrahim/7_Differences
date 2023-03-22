a6d16125521f112a4103f976b6fee2acffb85ace
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-03-21 22:52:00 -0400

    test: waiting-dialog done

--- client/src/app/components/waiting-player-to-join/waiting-player-to-join.component.spec.ts
+++ client/src/app/components/waiting-player-to-join/waiting-player-to-join.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -30,7 +30,7 @@ describe('WaitingPlayerToJoinComponent', () => {
             deletedGameId$: deletedGameIdMock,
         });
         clientSocketServiceSpy = jasmine.createSpyObj('ClientSocketService', ['send', 'disconnect']);
-        dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['afterClosed']);
+        dialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['afterClosed', 'close']);
         dialogRefSpy.afterClosed.and.returnValue(of('dialog closed'));
         router = jasmine.createSpyObj('Router', ['navigate']);
         await TestBed.configureTestingModule({
@@ -75,18 +75,31 @@ describe('WaitingPlayerToJoinComponent', () => {
         expect(countDownBeforeClosingSpy).toHaveBeenCalled();
     });
 
-    it('countDownBeforeClosing should set countdown', () => {
-        component['countdown'] = TEN_SECONDS;
-        component.ngOnInit();
-        component.countDownBeforeClosing();
-        deletedGameIdMock.next('test-game-id');
-        jasmine.clock().install();
-        jasmine.clock().tick(TEN_SECONDS);
-        component.countDownBeforeClosing();
-    });
+    // it('countDownBeforeClosing should set countdown', () => {
+    //     jasmine.clock().install();
+    //     component['countdown'] = TEN_SECONDS;
+    //     component.ngOnInit();
+    //     component.countDownBeforeClosing();
+    //     deletedGameIdMock.next('test-game-id');
+    //     expect(component['countdown']).toEqual(TEN_SECONDS);
+    //     jasmine.clock().tick(TEN_SECONDS);
+    //     expect(dialogRefSpy.close).not.toHaveBeenCalled();
+    // });
+
+    // it('countDownBeforeClosing should set countdown', () => {
+    //     jasmine.clock().install();
+    //     component['countdown'] = TEN_SECONDS;
+    //     component.ngOnInit();
+    //     component.countDownBeforeClosing();
+    //     deletedGameIdMock.next('test-game-id');
+    //     jasmine.clock().tick(TEN_SECONDS);
+    //     component.countDownBeforeClosing();
+    //     jasmine.clock().tick(TEN_SECONDS);
+    //     expect(dialogRefSpy.close).not.toHaveBeenCalled();
+    // });
 
     it('countDownBeforeClosing should set countdown', () => {
-        component['countdown'] = TEN_SECONDS;
+        component['countdown'] = 1;
         component.ngOnInit();
         component.countDownBeforeClosing();
         deletedGameIdMock.next('test-game-id');
@@ -94,7 +107,28 @@ describe('WaitingPlayerToJoinComponent', () => {
         jasmine.clock().tick(TEN_SECONDS);
         component.countDownBeforeClosing();
         jasmine.clock().tick(TEN_SECONDS);
+        expect(dialogRefSpy.close).not.toHaveBeenCalled();
     });
+
+    it('ngOnInit should call countDownBeforeClosing', fakeAsync(() => {
+        const countDownBeforeClosingSpy = spyOn(component, 'countDownBeforeClosing');
+
+        component.ngOnInit();
+        deletedGameIdMock.next('test-game-id');
+        tick();
+
+        expect(countDownBeforeClosingSpy).toHaveBeenCalled();
+    }));
+
+    it('should start countdown and show message if player is not in playerNames', fakeAsync(() => {
+        component['data'] = { gameId: 'Charlie', player: 'testPlayer', roomId: 'testRoom' };
+        deletedGameIdMock.next('Charlie');
+        component.ngOnInit();
+        expect(component.countdown).toBe(TEN_SECONDS);
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers -- needed for test
+        tick(12000);
+        expect(dialogRefSpy.close).toHaveBeenCalled();
+    }));
 
     it('refusePlayer should refuse the player using the roomManagerService', () => {
         const gameId = '12';
