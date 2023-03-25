e91f78fdbb91bc0d93a9b0b312d3914cfbdb7d82
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-03-25 13:07:33 -0400

    fix: right quadrant position

--- client/src/app/services/hint-service/hint.service.ts
+++ client/src/app/services/hint-service/hint.service.ts
@@ -16,6 +16,8 @@ export class HintService {
 
     requestHint() {
         if (this.nAvailableHints > 0) {
+            console.log('requesting hint');
+            console.log(this.classicSystem.cheatDifferences);
             let hintQuadrant = this.getHintQuadrant(this.classicSystem.cheatDifferences, INITIAL_QUADRANT);
             if (this.nAvailableHints === DEFAULT_N_HINTS - 1) {
                 hintQuadrant = this.getHintQuadrant(this.classicSystem.cheatDifferences, hintQuadrant);
@@ -31,22 +33,35 @@ export class HintService {
         const halfWidth = (quadrant.topCorner.x - quadrant.bottomCorner.x) / 2;
         const halfHeight = (quadrant.topCorner.y - quadrant.bottomCorner.y) / 2;
         const quadrants: QuadrantPosition[] = [];
-        for (const coord of differencesCoordinates) {
+        for (let coord of differencesCoordinates) {
+            coord = { x: coord.x, y: quadrant.topCorner.y - coord.y };
             if (coord.x > halfWidth) {
                 if (!quadrants.includes(QuadrantPosition.First) && coord.y > halfHeight) {
+                    console.log('first');
+                    console.log(coord);
                     quadrants.push(QuadrantPosition.First);
                 } else if (!quadrants.includes(QuadrantPosition.Fourth) && coord.y > quadrant.bottomCorner.y) {
+                    console.log('forth');
+                    console.log(coord);
                     quadrants.push(QuadrantPosition.Fourth);
                 }
             } else if (coord.x > quadrant.bottomCorner.x) {
                 if (!quadrants.includes(QuadrantPosition.Second) && coord.y > halfHeight) {
+                    console.log('second');
+                    console.log(coord);
                     quadrants.push(QuadrantPosition.Second);
                 } else if (!quadrants.includes(QuadrantPosition.Third) && coord.y > quadrant.bottomCorner.y) {
+                    console.log('third');
+                    console.log(coord);
                     quadrants.push(QuadrantPosition.Third);
                 }
             }
         }
+        console.log('quadrants: ' + quadrants);
         const quadrantNumber: number = quadrants.length > 1 ? this.generateRandomNumber(0, quadrants.length - 1) : 0;
+        console.log('quadrantNumber: ' + quadrantNumber);
+        console.log('subQuadrants: ');
+        console.log(subQuadrants[quadrants[quadrantNumber]]);
         return subQuadrants[quadrantNumber];
     }
 
@@ -82,20 +97,20 @@ export class HintService {
         const halfHeight = (quadrant.topCorner.y - quadrant.bottomCorner.y) / 2;
         const subQuadrants: Quadrant[] = [
             {
-                bottomCorner: { x: quadrant.bottomCorner.x + halfWidth, y: quadrant.bottomCorner.y + halfHeight },
-                topCorner: { x: quadrant.topCorner.x, y: quadrant.topCorner.y },
-            },
-            {
-                bottomCorner: { x: quadrant.bottomCorner.x, y: quadrant.bottomCorner.y + halfHeight },
-                topCorner: { x: quadrant.bottomCorner.x + halfWidth, y: quadrant.topCorner.y },
+                bottomCorner: { x: quadrant.bottomCorner.x + halfWidth, y: quadrant.bottomCorner.y },
+                topCorner: { x: quadrant.topCorner.x, y: quadrant.bottomCorner.y + halfHeight },
             },
             {
                 bottomCorner: { x: quadrant.bottomCorner.x, y: quadrant.bottomCorner.y },
                 topCorner: { x: quadrant.bottomCorner.x + halfWidth, y: quadrant.bottomCorner.y + halfHeight },
             },
             {
-                bottomCorner: { x: quadrant.bottomCorner.x + halfWidth, y: quadrant.bottomCorner.y },
-                topCorner: { x: quadrant.topCorner.x, y: quadrant.bottomCorner.y + halfHeight },
+                bottomCorner: { x: quadrant.bottomCorner.x, y: quadrant.bottomCorner.y + halfHeight },
+                topCorner: { x: quadrant.bottomCorner.x + halfWidth, y: quadrant.topCorner.y },
+            },
+            {
+                bottomCorner: { x: quadrant.bottomCorner.x + halfWidth, y: quadrant.bottomCorner.y + halfHeight },
+                topCorner: { x: quadrant.topCorner.x, y: quadrant.topCorner.y },
             },
         ];
         return subQuadrants;
