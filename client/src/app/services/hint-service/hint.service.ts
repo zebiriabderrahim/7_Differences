92d77f0e3cc7bad802dc33d3c5f49203aa19998c
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-03-25 13:57:01 -0400

    fix: wrong quadrant displayed

--- client/src/app/services/hint-service/hint.service.ts
+++ client/src/app/services/hint-service/hint.service.ts
@@ -16,8 +16,6 @@ export class HintService {
 
     requestHint() {
         if (this.nAvailableHints > 0) {
-            console.log('requesting hint');
-            console.log(this.classicSystem.cheatDifferences);
             let hintQuadrant = this.getHintQuadrant(this.classicSystem.cheatDifferences, INITIAL_QUADRANT);
             if (this.nAvailableHints === DEFAULT_N_HINTS - 1) {
                 hintQuadrant = this.getHintQuadrant(this.classicSystem.cheatDifferences, hintQuadrant);
@@ -29,40 +27,20 @@ export class HintService {
 
     private getHintQuadrant(differencesCoordinates: Coordinate[], quadrant: Quadrant): Quadrant {
         const subQuadrants: Quadrant[] = this.getSubQuadrants(quadrant);
-        // TODO: fix duplicate halfzies
-        // const halfWidth = (quadrant.topCorner.x - quadrant.bottomCorner.x) / 2;
-        // const halfHeight = (quadrant.topCorner.y - quadrant.bottomCorner.y) / 2;
         const quadrants: QuadrantPosition[] = [];
         for (const coord of differencesCoordinates) {
-            // coord = { x: coord.x, y: quadrant.topCorner.y - coord.y };
-            if (coord.x > subQuadrants[QuadrantPosition.First].bottomCorner.x) {
-                if (!quadrants.includes(QuadrantPosition.Fourth) && coord.y > subQuadrants[QuadrantPosition.Fourth].bottomCorner.y) {
-                    console.log('forth');
-                    console.log(coord);
-                    quadrants.push(QuadrantPosition.Fourth);
-                } else if (!quadrants.includes(QuadrantPosition.First) && coord.y > subQuadrants[QuadrantPosition.First].bottomCorner.y) {
-                    console.log('first');
-                    console.log(coord);
-                    quadrants.push(QuadrantPosition.First);
-                }
-            } else if (coord.x > subQuadrants[QuadrantPosition.Second].bottomCorner.x) {
-                if (!quadrants.includes(QuadrantPosition.Third) && coord.y > subQuadrants[QuadrantPosition.Third].bottomCorner.x) {
-                    console.log('third');
-                    console.log(coord);
-                    quadrants.push(QuadrantPosition.Third);
-                } else if (!quadrants.includes(QuadrantPosition.Second) && coord.y > subQuadrants[QuadrantPosition.Second].bottomCorner.x) {
-                    console.log('second');
-                    console.log(coord);
-                    quadrants.push(QuadrantPosition.Second);
-                }
+            if (!quadrants.includes(QuadrantPosition.First) && this.isPointInQuadrant(coord, subQuadrants[QuadrantPosition.First])) {
+                quadrants.push(QuadrantPosition.First);
+            } else if (!quadrants.includes(QuadrantPosition.Second) && this.isPointInQuadrant(coord, subQuadrants[QuadrantPosition.Second])) {
+                quadrants.push(QuadrantPosition.Second);
+            } else if (!quadrants.includes(QuadrantPosition.Third) && this.isPointInQuadrant(coord, subQuadrants[QuadrantPosition.Third])) {
+                quadrants.push(QuadrantPosition.Third);
+            } else if (!quadrants.includes(QuadrantPosition.Fourth) && this.isPointInQuadrant(coord, subQuadrants[QuadrantPosition.Fourth])) {
+                quadrants.push(QuadrantPosition.Fourth);
             }
         }
-        console.log('quadrants: ' + quadrants);
         const quadrantNumber: number = quadrants.length > 1 ? this.generateRandomNumber(0, quadrants.length - 1) : 0;
-        console.log('quadrantNumber: ' + quadrantNumber);
-        console.log('subQuadrants: ');
-        console.log(subQuadrants[quadrants[quadrantNumber]]);
-        return subQuadrants[quadrantNumber];
+        return subQuadrants[quadrants[quadrantNumber]];
     }
 
     private generateRandomNumber(min: number, max: number) {
@@ -71,7 +49,6 @@ export class HintService {
 
     private generateHintSquare(quadrant: Quadrant): Coordinate[] {
         const hintSquare: Coordinate[] = [];
-
         for (let j = quadrant.bottomCorner.y; j < quadrant.topCorner.y; j++) {
             for (let i = quadrant.bottomCorner.x; i < quadrant.bottomCorner.x + HINT_SQUARE_PADDING; i++) {
                 hintSquare.push({ x: i, y: j });
@@ -106,12 +83,12 @@ export class HintService {
         const halfHeight = (quadrant.topCorner.y - quadrant.bottomCorner.y) / 2;
         const subQuadrants: Quadrant[] = [
             {
-                bottomCorner: { x: quadrant.bottomCorner.x + halfWidth, y: quadrant.bottomCorner.y },
-                topCorner: { x: quadrant.topCorner.x, y: quadrant.bottomCorner.y + halfHeight },
-            },
-            {
                 bottomCorner: { x: quadrant.bottomCorner.x, y: quadrant.bottomCorner.y },
                 topCorner: { x: quadrant.bottomCorner.x + halfWidth, y: quadrant.bottomCorner.y + halfHeight },
+            },
+            {
+                bottomCorner: { x: quadrant.bottomCorner.x + halfWidth, y: quadrant.bottomCorner.y },
+                topCorner: { x: quadrant.topCorner.x, y: quadrant.bottomCorner.y + halfHeight },
             },
             {
                 bottomCorner: { x: quadrant.bottomCorner.x, y: quadrant.bottomCorner.y + halfHeight },
