6611c66b06c20035eaf40afc65eb47c4438293b0
ZakariaZair <zakaria.zair@polymtl.ca>
2023-03-28 11:59:11 -0400

    feat: changing replay interval interface in client side interfaces

--- client/src/app/services/replay-service/replay.service.spec.ts
+++ client/src/app/services/replay-service/replay.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { ReplayService } from './replay.service';
 
 describe('ReplayService', () => {
-  let service: ReplayService;
+    let service: ReplayService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ReplayService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(ReplayService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
