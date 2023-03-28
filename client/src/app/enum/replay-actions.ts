dc49a08ccd50b9292cef45b6ecdb2d01eb922411
ZakariaZair <zakaria.zair@polymtl.ca>
2023-03-28 12:11:25 -0400

    feat: move Action enum to enum folder

---
+++ client/src/app/enum/replay-actions.ts
@@ -0,0 +1,8 @@
+export enum Action {
+    ClicDiffFound = 'ClicDiffFound',
+    ClicError = 'ClicError',
+    CaptureMessage = 'CaptureMessage',
+    ActivateCheat = 'ActivateCheat',
+    DeactivateCheat = 'DeactivateCheat',
+    UseHint = 'UseHint',
+}
