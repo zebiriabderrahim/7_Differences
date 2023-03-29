caa24fefa57bfd1139e7c81c1505ceac6b343332
zebiriabderrahim <77996269+zebiriabderrahim@users.noreply.github.com>
2023-03-29 04:02:07 -0400

    fix: add delay before closing db  connection

--- server/app/services/database/database.service.spec.ts
+++ server/app/services/database/database.service.spec.ts
@@ -89,7 +89,7 @@ describe('DatabaseService', () => {
             thumbnail: 'assets/test/modified.bmp',
         },
     ];
-    const DELAY_BEFORE_CLOSING_CONNECTION = 10;
+    const DELAY_BEFORE_CLOSING_CONNECTION = 20;
 
     beforeEach(async () => {
         mongoServer = await MongoMemoryServer.create();
