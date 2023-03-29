9c33f038a337207cbb136fc4362dcd580394ab4e
zebiriabderrahim <77996269+zebiriabderrahim@users.noreply.github.com>
2023-03-29 03:09:53 -0400

    test: update data base service and game service and controller tests

--- server/app/services/players-list-manager/players-list-manager.service.spec.ts
+++ server/app/services/players-list-manager/players-list-manager.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { createStubInstance, SinonStubbedInstance } from 'sinon';
+import { GameService } from '@app/services/game/game.service';
 import { PlayersListManagerService } from './players-list-manager.service';
 
 describe('PlayersListManagerService', () => {
     let service: PlayersListManagerService;
+    let gameService: SinonStubbedInstance<GameService>;
 
     beforeEach(async () => {
+        gameService = createStubInstance(GameService);
         const module: TestingModule = await Test.createTestingModule({
-            providers: [PlayersListManagerService],
+            providers: [
+                PlayersListManagerService,
+                {
+                    provide: GameService,
+                    useValue: gameService,
+                },
+            ],
         }).compile();
 
         service = module.get<PlayersListManagerService>(PlayersListManagerService);
