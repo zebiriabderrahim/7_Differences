a45bc10f4340140948d98e92dde8e8f205c4c797
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-03-29 21:38:00 -0400

    feat: created config component

---
+++ client/src/app/components/config-dialog/config-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-config-dialog',
+  templateUrl: './config-dialog.component.html',
+  styleUrls: ['./config-dialog.component.scss']
+})
+export class ConfigDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
