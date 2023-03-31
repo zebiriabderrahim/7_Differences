ab2352637fae881efed722d832b35fac67043c19
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-03-31 16:34:14 -0400

    test: less errors

--- client/src/app/components/delete-reset-confirmation-dialog/delete-reset-confirmation-dialog.component.spec.ts
+++ client/src/app/components/delete-reset-confirmation-dialog/delete-reset-confirmation-dialog.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { DeleteResetConfirmationDialogComponent } from './delete-reset-confirmation-dialog.component';
 
 describe('DeleteResetConfirmationDialogComponent', () => {
@@ -9,6 +9,11 @@ describe('DeleteResetConfirmationDialogComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [DeleteResetConfirmationDialogComponent],
+            imports: [MatDialogModule],
+            providers: [
+                { provide: MAT_DIALOG_DATA, useValue: {} },
+                { provide: MatDialogRef, useValue: {} },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(DeleteResetConfirmationDialogComponent);
--- client/src/app/pages/limited-time-page/limited-time-page.component.spec.ts
+++ client/src/app/pages/limited-time-page/limited-time-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { LimitedTimePageComponent } from './limited-time-page.component';
 
 describe('LimitedTimePageComponent', () => {
@@ -9,6 +9,7 @@ describe('LimitedTimePageComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [LimitedTimePageComponent],
+            imports: [RouterTestingModule],
         }).compileComponents();
 
         fixture = TestBed.createComponent(LimitedTimePageComponent);
