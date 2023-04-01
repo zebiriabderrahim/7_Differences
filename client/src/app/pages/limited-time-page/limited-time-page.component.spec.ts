9b0fa6da55500fcb853cffcfface770fe919ef9e
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-03-31 22:48:30 -0400

    fix: mat-icon + app-nav-bar import errors

--- client/src/app/pages/limited-time-page/limited-time-page.component.spec.ts
+++ client/src/app/pages/limited-time-page/limited-time-page.component.spec.ts
@@ -5,6 +5,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NavBarComponent } from '@app/components/nav-bar/nav-bar.component';
 import { PlayerNameDialogBoxComponent } from '@app/components/player-name-dialog-box/player-name-dialog-box.component';
 import { LimitedTimePageComponent } from './limited-time-page.component';
 
@@ -14,7 +15,7 @@ describe('LimitedTimePageComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [LimitedTimePageComponent, PlayerNameDialogBoxComponent],
+            declarations: [LimitedTimePageComponent, PlayerNameDialogBoxComponent, NavBarComponent],
             imports: [RouterTestingModule, MatDialogModule, BrowserAnimationsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
             providers: [
                 { provide: MAT_DIALOG_DATA, useValue: {} },
