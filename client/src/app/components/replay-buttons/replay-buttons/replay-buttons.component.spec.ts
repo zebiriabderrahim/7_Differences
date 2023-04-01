5d49ca15c541c485caf716a9df72a436bd9784c3
ZakariaZair <zakaria.zair@polymtl.ca>
2023-03-31 23:50:08 -0400

    test: fix lint

--- client/src/app/components/replay-buttons/replay-buttons/replay-buttons.component.spec.ts
+++ client/src/app/components/replay-buttons/replay-buttons/replay-buttons.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReplayButtonsComponent } from './replay-buttons.component';
 
 describe('ReplayButtonsComponent', () => {
-  let component: ReplayButtonsComponent;
-  let fixture: ComponentFixture<ReplayButtonsComponent>;
+    let component: ReplayButtonsComponent;
+    let fixture: ComponentFixture<ReplayButtonsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ReplayButtonsComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ReplayButtonsComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(ReplayButtonsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(ReplayButtonsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
