5e8c8bc4ee0c4e99cb7fe71c3f4605d823fdbc1f
Edgar Kappauf <edgar.kappauf@polymtl.ca>
2023-04-10 13:38:29 -0400

    refactor: changed isImageValid

--- client/src/app/services/validation-service/validation.service.ts
+++ client/src/app/services/validation-service/validation.service.ts
@@ -6,19 +6,11 @@ import { IMG_HEIGHT, IMG_TYPE, IMG_WIDTH, VALID_BMP_SIZE } from '@app/constants/
 })
 export class ValidationService {
     async isImageValid(file: File): Promise<boolean> {
-        return this.isImageTypeValid(file) && (await this.isImageSizeValid(file)) && this.isImageFormatValid(file);
-    }
-
-    isImageTypeValid(file: File): boolean {
-        return file.type === IMG_TYPE;
+        return file.type === IMG_TYPE && (await this.isImageSizeValid(file)) && file.size === VALID_BMP_SIZE;
     }
 
     async isImageSizeValid(file: File): Promise<boolean> {
         const image = await createImageBitmap(file);
         return image.width === IMG_WIDTH && image.height === IMG_HEIGHT;
-    }
-
-    isImageFormatValid(file: File): boolean {
-        return file.size === VALID_BMP_SIZE;
     }
 }
