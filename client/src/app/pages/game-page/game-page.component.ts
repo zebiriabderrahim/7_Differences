bb01b384da3e7a2783ae08ac1a36df24189fa408
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-04-12 09:03:44 -0400

    style: renamed third hint method

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -220,7 +220,7 @@ export class GamePageComponent implements AfterViewInit, OnDestroy {
     mouseClickOnCanvas(event: MouseEvent, isLeft: boolean) {
         if (this.gameAreaService.detectLeftClick(event)) {
             if (this.isThirdHintActive) {
-                this.hintService.clickDuringThirdHint();
+                this.hintService.deactivateThirdHint();
             }
             this.gameAreaService.setAllData();
             this.classicService.setIsLeftCanvas(isLeft);
