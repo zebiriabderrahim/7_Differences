90c957632ecdff2327b85adab4f65c65155cbf75
zebiriabderrahim <77996269+zebiriabderrahim@users.noreply.github.com>
2023-04-12 15:16:06 -0400

    refactor: optimize code quality

--- client/src/app/components/game-infos/game-infos.component.ts
+++ client/src/app/components/game-infos/game-infos.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ClassicSystemService } from '@app/services/classic-system-service/classic-system.service';
 import { HintService } from '@app/services/hint-service/hint.service';
 import { ClientSideGame, GameConfigConst } from '@common/game-interfaces';
 @Component({
@@ -8,13 +9,15 @@ import { ClientSideGame, GameConfigConst } from '@common/game-interfaces';
 })
 export class GameInfosComponent {
     @Input() game: ClientSideGame;
-    @Input() differencesCount: number;
     @Input() isReplayAvailable: boolean;
-    @Input() gameConstants: GameConfigConst;
-    constructor(private readonly hintService: HintService) {}
+
+    constructor(private readonly hintService: HintService, private readonly classicService: ClassicSystemService) {}
 
     get nHints(): number {
         return this.hintService.nAvailableHints;
+    }
+    get gameConstants(): GameConfigConst {
+        return this.classicService.gameConstants;
     }
 
     requestHint() {
