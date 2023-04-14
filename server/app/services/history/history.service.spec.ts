7f5ce0db4bfec0501338884120bd78cfbb3303d5
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-04-13 23:38:59 -0400

    test: added HistoryService stub for server tests

--- server/app/services/history/history.service.spec.ts
+++ server/app/services/history/history.service.spec.ts
@@ -1,12 +1,22 @@
+import { GameService } from '@app/services/game/game.service';
 import { Test, TestingModule } from '@nestjs/testing';
+import { SinonStubbedInstance, createStubInstance } from 'sinon';
 import { HistoryService } from './history.service';
 
 describe('HistoryService', () => {
     let service: HistoryService;
+    let gameService: SinonStubbedInstance<GameService>;
 
     beforeEach(async () => {
+        gameService = createStubInstance(GameService);
         const module: TestingModule = await Test.createTestingModule({
-            providers: [HistoryService],
+            providers: [
+                HistoryService,
+                {
+                    provide: GameService,
+                    useValue: gameService,
+                },
+            ],
         }).compile();
 
         service = module.get<HistoryService>(HistoryService);
