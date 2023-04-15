83f42e90a9bb26785df8ba2269ba0cab41924113
ZakariaZair <zakaria.zair@polymtl.ca>
2023-04-15 18:47:46 -0400

    feat: added background music

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SoundService } from '@app/services/sound-service/sound.service';
 
 @Component({
     selector: 'app-main-page',
@@ -12,12 +13,13 @@ export class MainPageComponent {
     readonly configRoute: string;
     readonly teamNumber: string;
     readonly teammateNameList: string[];
-    constructor() {
+    constructor(private readonly soundService: SoundService) {
         this.gameTitle = '7 Différences';
         this.selectionRoute = '/selection';
         this.configRoute = '/config';
         this.limitedRoute = '/limited';
         this.teamNumber = "Présenté par l'équipe 101";
         this.teammateNameList = ['Jeremy Ear,', 'Sulayman Hosna,', 'Edgar Kappauf,', 'Mathieu Prévost,', 'Zakaria Zair,', 'Abderrahim Zebiri'];
+        this.soundService.loopBackgroundMusic();
     }
 }
