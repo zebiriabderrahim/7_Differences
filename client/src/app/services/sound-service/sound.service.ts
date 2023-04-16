923a247c333bf52097678509804d3fb3cc94a9fd
ZakariaZair <zakaria.zair@polymtl.ca>
2023-04-15 22:24:31 -0400

    feat: reduce volume

--- client/src/app/services/sound-service/sound.service.ts
+++ client/src/app/services/sound-service/sound.service.ts
@@ -15,7 +15,9 @@ export class SoundService {
         this.backgroundMusic = new Audio('assets/sound/BackgroundMusic.mp3');
         this.gameMusic = new Audio('assets/sound/GameMusic.mp3');
         this.backgroundMusic.loop = true;
+        this.backgroundMusic.volume = 0.02;
         this.gameMusic.loop = true;
+        this.gameMusic.volume = 0.02;
     }
 
     playErrorSound(): void {
@@ -42,5 +44,6 @@ export class SoundService {
     stopGameMusic(): void {
         this.gameMusic.pause();
         this.gameMusic.currentTime = 0;
+        this.backgroundMusic.play();
     }
 }
