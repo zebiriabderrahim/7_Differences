f27783b5c21cc5239f3d064355ab91cfbe284bab
ZakariaZair <zakaria.zair@polymtl.ca>
2023-04-15 21:01:30 -0400

    feat: added game music

--- client/src/app/services/sound-service/sound.service.ts
+++ client/src/app/services/sound-service/sound.service.ts
@@ -7,11 +7,15 @@ export class SoundService {
     private correctSoundEffect: HTMLAudioElement;
     private incorrectSoundEffect: HTMLAudioElement;
     private backgroundMusic: HTMLAudioElement;
+    private gameMusic: HTMLAudioElement;
 
     constructor() {
         this.correctSoundEffect = new Audio('assets/sound/WinSoundEffect.mp3');
         this.incorrectSoundEffect = new Audio('assets/sound/ErrorSoundEffect.mp3');
-        this.backgroundMusic = new Audio('assets/sound/DingDongMusic.mp3');
+        this.backgroundMusic = new Audio('assets/sound/BackgroundMusic.mp3');
+        this.gameMusic = new Audio('assets/sound/GameMusic.mp3');
+        this.backgroundMusic.loop = true;
+        this.gameMusic.loop = true;
     }
 
     playErrorSound(): void {
@@ -23,12 +27,20 @@ export class SoundService {
     }
 
     loopBackgroundMusic(): void {
-        this.backgroundMusic.loop = true;
         this.backgroundMusic.play();
     }
 
     stopBackgroundMusic(): void {
         this.backgroundMusic.pause();
         this.backgroundMusic.currentTime = 0;
+    }
+
+    loopGameMusic(): void {
+        this.gameMusic.play();
+    }
+
+    stopGameMusic(): void {
+        this.gameMusic.pause();
+        this.gameMusic.currentTime = 0;
     }
 }
