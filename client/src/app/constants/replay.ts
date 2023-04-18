fd684b09cb389e11f40efbfb410cb13d8d47043f
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-04-17 23:38:31 -0400

    refactor: removed code duplication replay-buttons

--- client/src/app/constants/replay.ts
+++ client/src/app/constants/replay.ts
@@ -2,4 +2,6 @@ export const SPEED_X1 = 1;
 export const SPEED_X2 = 2;
 export const SPEED_X4 = 4;
 
+export const REPLAY_SPEEDS = [SPEED_X1, SPEED_X2, SPEED_X4];
+
 export const REPLAY_LIMITER = 1000;
