bed3938e0186ca33821d770670d7d894aba06c3e
zebiriabderrahim <77996269+zebiriabderrahim@users.noreply.github.com>
2023-04-18 01:02:13 -0400

    refactor: delete unused parameter

--- server/app/services/players-list-manager/players-list-manager.service.ts
+++ server/app/services/players-list-manager/players-list-manager.service.ts
@@ -32,7 +32,7 @@ export class PlayersListManagerService {
         this.cancelJoiningByPlayerName(playerPayLoad.playerName, playerPayLoad.gameId, server);
     }
 
-    getAcceptPlayer(gameId: string, playerName: string, server: io.Server): Player {
+    getAcceptPlayer(gameId: string, server: io.Server): Player {
         const acceptedPlayer = this.joinedPlayersByGameId.get(gameId)?.[0];
         if (!acceptedPlayer) return;
         this.cancelAllJoining(gameId, server);
@@ -67,10 +67,10 @@ export class PlayersListManagerService {
     }
 
     deleteJoinedPlayerByPlayerId(playerId: string, gameId: string) {
-        const playerNames = this.joinedPlayersByGameId?.get(gameId);
+        const playerNames = this.joinedPlayersByGameId.get(gameId);
+        if (!playerNames) return;
         const playerIndex = playerNames.findIndex((player) => player.playerId === playerId);
-        if (playerIndex === NOT_FOUND) return;
-        playerNames.splice(playerIndex, 1);
+        if (playerIndex !== NOT_FOUND) playerNames.splice(playerIndex, 1);
         this.joinedPlayersByGameId.set(gameId, playerNames);
     }
 
