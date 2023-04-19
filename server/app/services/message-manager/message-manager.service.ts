ca26b1cbcc2a249afab76d1f38891360436668ae
Mathieu Prevost <mathieu-1.prevost@polymtl.ca>
2023-04-19 06:45:41 -0400

    refactor: message-manager simplified

--- server/app/services/message-manager/message-manager.service.ts
+++ server/app/services/message-manager/message-manager.service.ts
@@ -5,61 +5,29 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MessageManagerService {
-    private time: Date;
-
     getQuitMessage(playerName: string): ChatMessage {
-        const localMessage: ChatMessage = {
-            tag: MessageTag.Common,
-            message: this.getFormatTime() + ` - ${playerName} a abandonné la partie`,
-        };
-        return localMessage;
+        return this.createMessage(MessageTag.Common, `${playerName} a abandonné la partie`);
     }
 
     getNewRecordMessage(newRecord: NewRecord): ChatMessage {
-        const localMessage: ChatMessage = {
-            tag: MessageTag.Global,
-            message:
-                this.getFormatTime() +
-                ` – ${newRecord.playerName} est maintenant ${SCORE_POSITION[newRecord.rank]}` +
-                ` dans les meilleurs temps du jeu ${newRecord.gameName} en ${newRecord.gameMode}`,
-        };
-        return localMessage;
+        const content =
+            `${newRecord.playerName} est maintenant ${SCORE_POSITION[newRecord.rank]}` +
+            ` dans les meilleurs temps du jeu ${newRecord.gameName} en ${newRecord.gameMode}`;
+        return this.createMessage(MessageTag.Global, content);
     }
 
-    getLocalMessage(gameMode: string, isFound: boolean, playerName: string): ChatMessage {
-        if (isFound) {
-            return gameMode === GameModes.ClassicSolo ? this.getDifferenceMessage() : this.getDifferenceMessage(playerName);
-        } else {
-            return gameMode === GameModes.ClassicSolo ? this.getErrorMessage() : this.getErrorMessage(playerName);
+    getLocalMessage(gameMode: string, isDifferenceFound: boolean, playerName: string): ChatMessage {
+        let content = isDifferenceFound ? 'Différence trouvée' : 'Erreur';
+        if (gameMode !== GameModes.ClassicSolo && gameMode !== GameModes.LimitedSolo) {
+            content += ` par ${playerName}`;
         }
+        return this.createMessage(MessageTag.Common, content);
     }
 
-    private getFormatTime(): string {
-        this.time = new Date();
-        return `${this.time.getHours()} : ${this.time.getMinutes()} : ${this.time.getSeconds()}`;
-    }
-
-    private appendPlayerName(localMessage: ChatMessage, playerName?: string): ChatMessage {
-        if (playerName) {
-            localMessage.message += ` par ${playerName}`;
-        }
-        return localMessage;
-    }
-
-    private getDifferenceMessage(playerName?: string): ChatMessage {
-        const localMessage: ChatMessage = this.basicMessage('Différence trouvée');
-        return this.appendPlayerName(localMessage, playerName);
-    }
-
-    private getErrorMessage(playerName?: string): ChatMessage {
-        const localMessage: ChatMessage = this.basicMessage('Erreur');
-        return this.appendPlayerName(localMessage, playerName);
-    }
-
-    private basicMessage(message: string): ChatMessage {
-        return {
-            tag: MessageTag.Common,
-            message: this.getFormatTime() + ' - ' + message,
-        };
+    private createMessage(tag: MessageTag, content: string): ChatMessage {
+        const date: Date = new Date();
+        const time = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
+        const message = time + ' - ' + content;
+        return { tag, message };
     }
 }
